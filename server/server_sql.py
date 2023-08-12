de798716335acfd94609ec715e79fd9d0efc8d23
Patrick <>
2023-08-12 14:02:36 +0200

    idk

--- server/server_sql.py
+++ server/server_sql.py
@@ -45,10 +45,8 @@ log.addHandler(log_fh)
 # Path of server.py
 server_dir = os.path.dirname(os.path.realpath(__file__))
 
-dbdir = server_dir + "/users.db"
-
 # Connect to the database
-db = sql.connect('users.db', check_same_thread=False)
+db = sql.connect(server_dir + "users.db", check_same_thread=False)
 c = db.cursor()
 
 with open(server_dir + "/config.json", "r") as f:
