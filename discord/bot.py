928f3b0f7de9ed9a03ad63a5c138df83ec6dbcd6
Juliandev02 <juli.mackenstein@gmail.com>
2023-08-14 22:19:24 +0200

    Uploaded first beta test of discord bridge (dont work + DO NOT USE!)

---
+++ discord/bot.py
@@ -0,0 +1,27 @@
+import discord
+import asyncio
+import configparser
+from datetime import datetime
+import yaml
+from yaml import SafeLoader
+from discord.ext import commands
+
+with open("config.yml", encoding="utf-8") as config:
+        conf = yaml.load(config, Loader=SafeLoader)
+
+bot = commands.Bot(command_prefix='.',intents=discord.Intents.all())
+
+
+class DiscordBot(bot):
+    def start_bot(self):
+        try:
+            self.run(conf["bot"]["token"])
+            
+        except discord.errors.LoginFailure:
+            print("Login failed")
+            
+    async def on_ready(self):
+        print("Logged in")
+
+    async def on_message(self, message, edited=False):
+        pass
