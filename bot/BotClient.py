533de0be7237d79564c987bfb9b2b413b3b0b813
Juliandev02 <juli.mackenstein@gmail.com>
2023-08-17 00:00:38 +0200

    First good public upload of the scapi bot api

--- bot/BotClient.py
+++ bot/BotClient.py
@@ -1,3 +1,16 @@
 import yaml
 from yaml import SafeLoader
-import scapi
+from scapi import Scapi
+
+with open("config.yml", encoding="utf-8") as config:
+    conf = yaml.load(config, Loader=SafeLoader)
+
+username    = conf["bot"]["username"]
+token       = conf["bot"]["token"]
+host        = conf["server"]["host"]
+port        = conf["server"]["port"]
+
+Bot = Scapi.Bot(username=username, token=token, host=host, port=port, prefix="!")
+Bot.login()
+Bot.flagHandler(printReceivedMessagesToTerminal=True, enableUserInput=True)
+Bot.run()
