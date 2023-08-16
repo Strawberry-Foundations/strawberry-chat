68ac9862eb9842c76bc7fa570bf0469504873b0a
Juliandev02 <juli.mackenstein@gmail.com>
2023-08-16 17:37:59 +0200

    scapi api

---
+++ bot/scapi.py
@@ -0,0 +1,16 @@
+import sys
+import socket
+import threading
+import time
+
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+RESET = '\033[0m'
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+PURPLE = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
