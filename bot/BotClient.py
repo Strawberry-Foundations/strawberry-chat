20ad9eb5d4b495c18a7122d4f9cd81ed391c2f73
Juliandev02 <juli.mackenstein@gmail.com>
2023-08-17 17:27:55 +0200

    updated bot client

--- bot/BotClient.py
+++ bot/BotClient.py
@@ -1,6 +1,7 @@
 import yaml
 from yaml import SafeLoader
 from scapi import Scapi
+import threading
 
 with open("config.yml", encoding="utf-8") as config:
     conf = yaml.load(config, Loader=SafeLoader)
@@ -13,4 +14,21 @@ port        = conf["server"]["port"]
 Bot = Scapi.Bot(username=username, token=token, host=host, port=port, prefix="!")
 Bot.login()
 Bot.flagHandler(printReceivedMessagesToTerminal=True, enableUserInput=True)
-Bot.run()
+
+
+def Commands():
+    while True:
+        message = Bot.recv_message(raw=True)
+    
+        match message:
+            case "Hallo":
+                Bot.send_message("Hallo :D")
+            
+            case "help":
+                Bot.send_message("Das kann ich noch nicht. Wie wärs wenn du dir mal die eingebauten Commands anschaust?")
+
+BotThread = threading.Thread(target=Bot.run)
+CommandThread = threading.Thread(target=Commands)
+
+BotThread.start()
+CommandThread.start()
