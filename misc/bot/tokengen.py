eaf5964a50fdd93fe63fdf3e10c07142bd1ee4b4
Juliandev02 <juli.mackenstein@gmail.com>
2023-08-17 17:50:53 +0200

    new token form factor

--- misc/bot/tokengen.py
+++ misc/bot/tokengen.py
@@ -9,14 +9,15 @@ def generate_token():
         pw_part_1 += random.choice(characters)
         pw_part_2 += random.choice(characters)
         pw_part_3 += random.choice(characters)
-    
-    password = pw_part_1 + "-" + pw_part_2 + "-" + pw_part_3
+        pw_part_4 += random.choice(characters)
+
+    password = pw_part_1 + "_" + pw_part_2 + "." + pw_part_3 + "_" + pw_part_4
     return password
 
 def main():
-  """Generates and prints the bot token."""
-  password = generate_token()
-  print(password)
+    """Generates and prints the bot token."""
+    password = generate_token()
+    print(password)
 
 if __name__ == "__main__":
-  main()
+    main()
