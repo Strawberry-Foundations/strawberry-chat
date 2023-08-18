77071afdd15f23edac452dde35fbd8041e1f04c5
Juliandev02 <juli.mackenstein@gmail.com>
2023-08-18 23:50:17 +0200

    Renamed BotClient.py to bot.py

---
+++ bot/bot.py
@@ -0,0 +1,45 @@
+import scapi
+from scapi import Scapi
+
+import yaml
+from yaml import SafeLoader
+import threading
+
+with open("config.yml", encoding="utf-8") as config:
+    conf = yaml.load(config, Loader=SafeLoader)
+
+username    = conf["bot"]["username"]
+token       = conf["bot"]["token"]
+host        = conf["server"]["host"]
+port        = conf["server"]["port"]
+prefix      = conf["bot"]["prefix"]
+
+Bot = Scapi.Bot(username=username, token=token, host=host, port=port)
+Bot.login()
+Bot.flagHandler(printReceivedMessagesToTerminal=True, enableUserInput=True)
+
+
+def Commands():
+    while True:
+        message = Bot.recv_message(raw=True)
+    
+        match message:
+            case "Hallo":
+                Bot.send_message("Hallo :D")
+            
+            case "!help":
+                Bot.send_message("Noch nicht implementiert. Wie wärs wenn du dir mal die eingebauten Commands anschaust? Nutze dafür /help")
+            
+            case "!about":
+                Bot.send_message(f"{Bot.username} Bot BETA VERSION! Not finished yet")
+
+
+@Bot.event
+def on_ready():
+    print(f"{Bot.log_msg}{scapi.BLUE}{Bot.username} started successfully!{scapi.RESET}")
+    
+BotThread = threading.Thread(target=Bot.run, args=(on_ready,))
+CommandThread = threading.Thread(target=Commands)
+
+BotThread.start()
+CommandThread.start()
