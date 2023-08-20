3a183219820e5ffddea6b55ca57094198954beeb
Juliandev02 <juli.mackenstein@gmail.com>
2023-08-20 17:33:15 +0200

    Updated color variables

---
+++ server/src/colors.py
@@ -0,0 +1,27 @@
+from colorama import Fore
+
+# Alias for colorama colors
+BLACK           = Fore.BLACK
+RED             = Fore.RED
+GREEN           = Fore.GREEN
+YELLOW          = Fore.YELLOW
+BLUE            = Fore.BLUE
+MAGENTA         = Fore.MAGENTA
+CYAN            = Fore.CYAN
+WHITE           = Fore.WHITE
+RESET           = Fore.RESET
+
+LIGHTBLACK_EX   = Fore.LIGHTBLACK_EX
+LIGHTRED_EX     = Fore.LIGHTRED_EX
+LIGHTGREEN_EX   = Fore.LIGHTGREEN_EX
+LIGHTYELLOW_EX  = Fore.LIGHTYELLOW_EX
+LIGHTBLUE_EX    = Fore.LIGHTBLUE_EX
+LIGHTMAGENTA_EX = Fore.LIGHTMAGENTA_EX
+LIGHTCYAN_EX    = Fore.LIGHTCYAN_EX
+LIGHTWHITE_EX   = Fore.LIGHTWHITE_EX
+
+class Colors:
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
+    GRAY = "\033[90m"
