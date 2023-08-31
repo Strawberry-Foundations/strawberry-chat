32cd9eefa94590a79fa19cf8eabf7296284586fc
Juliandev02 <juli.mackenstein@gmail.com>
2023-08-31 20:51:26 +0200

    Updated bard bot (some fixes)

--- bot/bard.py
+++ bot/bard.py
@@ -9,13 +9,12 @@ import threading
 with open("bard.yml", encoding="utf-8") as config:
     conf = yaml.load(config, Loader=SafeLoader)
 
-username    = conf["bot"]["username"]
-token       = conf["bot"]["token"]
-host        = conf["server"]["host"]
-port        = conf["server"]["port"]
-prefix      = conf["bot"]["prefix"]
-bard_token  = conf["bard"]["token"]
-
+username        = conf["bot"]["username"]
+token           = conf["bot"]["token"]
+host            = conf["server"]["host"]
+port            = conf["server"]["port"]
+prefix          = conf["bot"]["prefix"]
+bard_token      = conf["bard"]["token"]
 
 Bot = Scapi.Bot(username=username, token=token, host=host, port=port)
 Bot.login()
@@ -28,16 +27,35 @@ print(f"{Bot.log_msg}{scapi.BLUE}Successfully connected to the Bard API.{scapi.R
 def Commands():
     while True:
         try:
-            message = Bot.recv_message(raw=True)
+            message = Bot.recv_message(raw=False, ansi=False)
             
-            if message.startswith("!test"):
-                Bot.send_message("This is a test message")
+            index       = message.find(":")
+            raw_message = message[index + 2:]
             
-            elif message.startswith("!bard "):
-                arg = message.replace("!bard ", "")
-                response = bard.get_answer(arg)['content']
+            if raw_message.startswith("!test"):
+                allowed_users   = conf["bard"]["allowed_users"]
+                username    = Bot.get_username_by_msg(message)
+                print(username)
                 
-                Bot.send_message(response)
+                if str(username) == "julian":
+                    Bot.send_message("Youre allowed!")
+                    
+                else:
+                    Bot.send_message(f"Youre not allowed.. you are {username} and allowed are {allowed_users}")
+                
+            elif raw_message.startswith("!bard "):
+                allowed_users   = conf["bard"]["allowed_users"]
+                username    = Bot.get_username_by_msg(message)            
+                
+                if username in allowed_users:
+                    arg = raw_message.replace("!bard ", "")
+                    Bot.send_message("Generating your response... this can take a while...")
+                    response = bard.get_answer(arg)['content']
+                    
+                    Bot.send_message(response)
+                    
+                else:
+                    Bot.send_message(f"{scapi.RED}Sorry, you do not have permissons to do that.{scapi.RESET}")
                 
         except Exception as Error: 
             Bot.logger(f"{scapi.RED}An unknown exception occured{scapi.RESET}", type=Bot.type.error)
