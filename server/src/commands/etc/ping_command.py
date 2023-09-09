3b86dcea0cab1a600fb616183e110765185df199
Patrick <patrickk0@tutanota.com>
2023-09-09 16:04:04 +0200

    rename

--- server/src/commands/etc/ping_command.py
+++
@@ -1,6 +0,0 @@
-from .. import register_command
-
-
-@register_command("test")
-def test_command():
-    print("Hello, world")
---
+++ server/src/commands/etc/test_command.py
@@ -0,0 +1,9 @@
+import socket
+
+from .. import register_command
+
+
+@register_command("test", 2)
+def test_command(stream: socket.socket, username: str, args: list):
+    stream.send(f"username: {username}\n".encode("utf8"))
+    stream.send(f"args: {args}".encode("utf8"))
