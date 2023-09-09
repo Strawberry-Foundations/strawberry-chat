717397f4a5aeb8f61912df0ba8f558a879fbeac4
Juliandev02 <juli.mackenstein@gmail.com>
2023-09-09 20:53:27 +0200

    changed stream variable to socket

--- server/src/commands/etc/test_command.py
+++ server/src/commands/etc/test_command.py
@@ -3,6 +3,6 @@ from .. import register_command
 
 
 @register_command("test", 2)
-def test_command(stream: socket.socket, username: str, args: list):
-    stream.send(f"Username: {username}".encode("utf8"))
-    stream.send(f"Args: {args}".encode("utf8"))
+def test_command(socket: socket.socket, username: str, args: list):
+    socket.send(f"Username: {username}".encode("utf8"))
+    socket.send(f"Args: {args}".encode("utf8"))
