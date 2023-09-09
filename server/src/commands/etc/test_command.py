b6c2fed9105e5fadf5307a5d7588ba786c1dddd4
Juliandev02 <juli.mackenstein@gmail.com>
2023-09-09 20:45:11 +0200

    removed new line (\n)

--- server/src/commands/etc/test_command.py
+++ server/src/commands/etc/test_command.py
@@ -5,5 +5,5 @@ from .. import register_command
 
 @register_command("test", 2)
 def test_command(stream: socket.socket, username: str, args: list):
-    stream.send(f"username: {username}\n".encode("utf8"))
+    stream.send(f"username: {username}".encode("utf8"))
     stream.send(f"args: {args}".encode("utf8"))
