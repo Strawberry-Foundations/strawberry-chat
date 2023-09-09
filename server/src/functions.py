34d1d886196985876ad878cd8ba3b48595059faa
Juliandev02 <juli.mackenstein@gmail.com>
2023-09-09 13:20:00 +0200

    Added repl_htpf to replace placeholders from the Hashtag Placeholders Format

--- server/src/functions.py
+++ server/src/functions.py
@@ -18,6 +18,7 @@ import requests
 import re
 
 from colorama import Fore, Style
+from .colors import *
 
 # Receive your global ip address for verification
 def get_global_ip():
@@ -29,3 +30,25 @@ def get_global_ip():
 def escape_ansi(line):
     ansi_escape = re.compile(r'(?:\x1B[@-_]|[\x80-\x9F])[0-?]*[ -/]*[@-~]')
     return ansi_escape.sub('', line)
+
+def repl_htpf(str):
+    to_ret = str.replace("#red", RED
+                ).replace("#green", GREEN
+                ).replace("#yellow", YELLOW
+                ).replace("#blue", BLUE
+                ).replace("#magenta", MAGENTA
+                ).replace("#cyan", CYAN
+                ).replace("#white", WHITE
+                ).replace("#reset", RESET
+                ).replace("#bold", Colors.BOLD
+                ).replace("#underline", Colors.UNDERLINE
+                ).replace("#today", datetime.datetime.today().strftime("%Y-%m-%d")
+                ).replace("#curtime", datetime.datetime.now().strftime("%H:%M")
+                ).replace("#month", datetime.datetime.today().strftime("%m")
+                ).replace("#fullmonth", datetime.datetime.now().strftime("%h")
+                ).replace("#ftoday", datetime.datetime.now().strftime("%A, %d. %h %Y")
+                ).replace("#tomorrow", (datetime.date.today() + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+                ).replace("#ftomorrow", (datetime.date.today() + datetime.timedelta(days=1)).strftime("%A, %d. %h %Y")
+                )
+                
+    return to_ret
