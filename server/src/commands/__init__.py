03841359b7db1035309ccb6312cafbac4c9809da
Patrick <patrickk0@tutanota.com>
2023-09-09 16:06:42 +0200

    add permission check

--- server/src/commands/__init__.py
+++ server/src/commands/__init__.py
@@ -1,20 +1,37 @@
-from etc.ping_command import *
+import socket
+from enum import Enum
+
+
+class PermissionLevel(Enum):
+    MEMBER = 0
+    ADMIN = 1
+
 
 command_registry = {}
 
 
-def register_command(name, arg_count=0):
+def register_command(name, arg_count=0, required_permissions=PermissionLevel.MEMBER):
     def decorator(func):
         command_registry[name] = (func, arg_count)
         return func
+
     return decorator
 
 
-def execute_command(command_str):
+def execute_command(command_str, stream: socket.socket, user: str, user_perms: PermissionLevel, args: list):
     command_name = command_str
     if command_name in command_registry:
-        command_func, arg_count = command_registry[command_name]
+        cmd = command_registry[command_name]
+        if cmd[1] > args.__len__():
+            stream.send(f"Not enough arguments - command requires {cmd[1]} arguments but {args.__len__()} were given".encode("utf8"))
+            return
+        if user_perms.value < cmd[2]:
+            stream.send("You lack the permission to use this command!".encode("utf8"))
+            return
+        cmd[0](stream, user, args)
     else:
-        print(f"Command '{command_name}' not found.")
+        stream.send(f"Command '{command_name}' not found.".encode("utf8"))
 
-register_command("test",0)(ping_command.register_command())
+
+def list_commands():
+    print(command_registry)
