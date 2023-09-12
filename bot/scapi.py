f24d80239d5915d9a700e59381682304e3230236
Juliandev02 <juli.mackenstein@gmail.com>
2023-09-12 20:52:34 +0200

    reimported readline

--- bot/scapi.py
+++ bot/scapi.py
@@ -6,10 +6,10 @@ import sys
 import re
 from enum import Enum
 
-# if sys.platform == "linux":
-#     import readline
-# elif sys.platform == "win32":
-#     pass
+if sys.platform == "linux":
+    import readline
+elif sys.platform == "win32":
+    pass
 
 BOLD = '\033[1m'
 UNDERLINE = '\033[4m'
