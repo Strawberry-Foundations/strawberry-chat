ac07490f4770ae489351f8c5227ec6da09a1c6e9
Juliandev02 <juli.mackenstein@gmail.com>
2023-09-29 23:43:37 +0200

    Ported description command

---
+++ server/src/commands/user/description.py
@@ -0,0 +1,40 @@
+from .. import register_command
+
+import socket
+
+from src.colors import *
+from src.db import Database
+
+from init import server_dir
+
+@register_command("description", arg_count=0)
+def description_command(socket: socket.socket, username: str, args: list):
+    cmd_db = Database(server_dir + "/users.db", check_same_thread=False)
+    
+    desc = " ".join(args)
+    
+    if desc.lower() == "remove" or desc.lower() == "reset":
+        cmd_db.execute("UPDATE users SET description = NULL WHERE username = ?", (username,))
+        cmd_db.commit()
+        
+        socket.send(f"{LIGHTGREEN_EX + Colors.BOLD}Removed Description{RESET + Colors.RESET}".encode("utf8"))
+    
+    elif desc.lower() == "" or desc.lower() == " ":
+        cmd_db.execute("SELECT description FROM users WHERE username = ?", (username,))
+        desc = cmd_db.fetchone()[0]
+        
+        socket.send(f"{LIGHTGREEN_EX + Colors.BOLD}Your current description: {RESET}{desc}{Colors.RESET}".encode("utf8"))
+        
+    else:        
+        cmd_db.execute("UPDATE users SET description = ? WHERE username = ?", (desc, username))
+        cmd_db.commit()
+        
+        socket.send(f"{LIGHTGREEN_EX + Colors.BOLD}Changed Description to {CYAN}{desc}{RESET + Colors.RESET}".encode("utf8"))
+        
+
+@register_command("desc")
+def desc_command(socket: socket.socket, username: str, args: list):
+    cmd_db = Database(server_dir + "/users.db", check_same_thread=False)
+    cmd_db.execute("SELECT description FROM users WHERE username = ?", (username,))
+    desc = cmd_db.fetchone()[0]
+    socket.send(f"{LIGHTGREEN_EX + Colors.BOLD}Your current description: {RESET}{desc}{Colors.RESET}".encode("utf8"))                
