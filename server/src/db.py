f8a3e956a3ca6a9c949f6f83ba63cc0b061b2468
Juliandev02 <juli.mackenstein@gmail.com>
2023-09-29 20:36:11 +0200

    Test No. 2: Using own DB wrapper

--- server/src/db.py
+++ server/src/db.py
@@ -16,5 +16,8 @@ class Database:
         self.db_connection.commit()
     
     def close(self):
-        self.db_connection.close()
+        # self.db_connection.close()
         self.cursor.close()
+    
+    def cursor_close(self):
+        self.db_connection.close()
