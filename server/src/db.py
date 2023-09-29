ec612107affc332fcf14601590e2d193f7564797
Juliandev02 <juli.mackenstein@gmail.com>
2023-09-29 20:32:33 +0200

    Test No. 1: Using own DB wrapper

--- server/src/db.py
+++ server/src/db.py
@@ -4,18 +4,17 @@ import sys
 from init import * 
 
 
-class StbWrapper:
-    class DB:
-        def __init__(self, file):
-            self.db_connection = sql.connect(file, check_same_thread=False)
-            self.cursor = self.db_connection.cursor()
+class Database:
+    def __init__(self, database, check_same_thread: bool = ...):
+        self.db_connection = sql.connect(database, check_same_thread)
+        self.cursor = self.db_connection.cursor()
 
-        def execute(self, statement):
-            self.db_connection.execute(statement)
-        
-        def commit(self):
-            self.db_connection.commit()
-        
-        def close(self):
-            self.db_connection.close()
-            self.cursor.close()
+    def execute(self, statement):
+        self.cursor.execute(statement)
+    
+    def commit(self):
+        self.db_connection.commit()
+    
+    def close(self):
+        self.db_connection.close()
+        self.cursor.close()
