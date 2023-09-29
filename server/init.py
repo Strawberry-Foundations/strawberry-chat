fdabb1db5e8398ea74d8bb823f01ab3f15ce835f
Juliandev02 <juli.mackenstein@gmail.com>
2023-09-30 00:04:47 +0200

    Added config data to init.py

--- server/init.py
+++ server/init.py
@@ -1,7 +1,33 @@
 import os
+import yaml
+from yaml import SafeLoader
+
+from src.colors import YELLOW, Colors, RESET, GREEN
+from src.online import get_global_ip
 
 # Path of init.py
 server_dir = os.path.dirname(os.path.realpath(__file__))
 
 afks = list([])
 users = {}
+
+# Open Configuration
+with open(server_dir + "/config.yml") as config_data:
+        config = yaml.load(config_data, Loader=SafeLoader)
+
+# Configuration
+ipaddr                  = config['server']['address']
+port                    = config['server']['port']
+
+enable_messages         = config['flags']['enable_messages']
+max_message_length      = config['flags']['max_message_length']
+debug_mode              = config['flags']['debug_mode']
+online_mode             = config['flags']['online_mode']
+
+if online_mode:
+    print(f"{YELLOW + Colors.BOLD}>>> Connecting to the Strawberry API ...{RESET + Colors.RESET}")
+    global_ip           = get_global_ip()
+    print(f"{GREEN + Colors.BOLD}>>> Connected{RESET + Colors.RESET}")
+    
+else:
+    global_ip = ""
