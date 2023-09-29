1a202e15b1f8861c6bc8e902ebb814af5ac434a7
Juliandev02 <juli.mackenstein@gmail.com>
2023-09-29 22:57:39 +0200

    ported members command

---
+++ server/src/commands/user/members.py
@@ -0,0 +1,20 @@
+from .. import register_command
+
+import socket
+
+from src.colors import *
+from src.db import Database
+
+from init import server_dir
+
+@register_command("members")
+@register_command("users")
+def members_command(socket: socket.socket, username: str, args: list):
+    cmd_db = Database(server_dir + "/users.db", check_same_thread=False)
+    cmd_db.execute("SELECT username FROM users")
+    raw_members = cmd_db.fetchall()
+    membersLen = len([raw_members for raw_members in sorted(raw_members)])
+    members = ", ".join([result[0] for result in raw_members])
+
+    socket.send(f"""{CYAN +  Colors.UNDERLINE + Colors.BOLD}Members on this server ({membersLen}){RESET + Colors.RESET}
+        {Colors.BOLD}->{Colors.RESET} {CYAN}{members}{RESET}""".encode("utf8"))
