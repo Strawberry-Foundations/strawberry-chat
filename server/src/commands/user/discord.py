da33de6e0621c2349b3a1c7e483b92f96e2a0dec
Juliandev02 <juli.mackenstein@gmail.com>
2023-09-30 14:54:43 +0200

    Ported discord command

---
+++ server/src/commands/user/discord.py
@@ -0,0 +1,25 @@
+from .. import register_command
+
+import socket
+
+from src.colors import *
+from src.db import Database
+
+from init import server_dir
+
+@register_command("discord", arg_count=1)
+def discord_command(socket: socket.socket, username: str, args: list):
+    cmd_db = Database(server_dir + "/users.db", check_same_thread=False)
+    
+    discord_name = args[0]
+
+    if discord_name.lower() == "remove":
+        cmd_db.execute("UPDATE users SET discord_name = NULL WHERE username = ?", (username,))
+        cmd_db.commit()
+        
+        socket.send(f"{LIGHTGREEN_EX + Colors.BOLD}Removed Discord Link{RESET + Colors.RESET}".encode("utf8"))
+    
+    cmd_db.execute("UPDATE users SET discord_name = ? WHERE username = ?", (discord_name, username))
+    cmd_db.commit()
+    
+    socket.send(f"{LIGHTGREEN_EX + Colors.BOLD}Changed Discord Link to {MAGENTA}{discord_name}{RESET + Colors.RESET}".encode("utf8"))
