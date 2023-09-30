0fb2e0539291a3bcbc5835eba96ce53fd0e61879
Juliandev02 <juli.mackenstein@gmail.com>
2023-09-30 19:46:33 +0200

    Updated versions

--- server/src/vars.py
+++ server/src/vars.py
@@ -2,7 +2,7 @@ from .colors import *
 
 # Version-specified Variables & important variables
 base_ver                = "1.8.0"
-short_ver               = "1.8.0b4+u1"
+short_ver               = "1.8.0b4+u2"
 full_ver                = "v" + short_ver
 update_channel          = "dev"
 ver                     = short_ver + "-vacakes"
