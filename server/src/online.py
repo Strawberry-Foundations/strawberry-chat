6d7a39815636e060256cb2f07858331c08c74550
Juliandev02 <juli.mackenstein@gmail.com>
2023-09-30 19:43:15 +0200

    added update function

--- server/src/online.py
+++ server/src/online.py
@@ -1,21 +1,25 @@
 import requests
-from src.vars import short_ver
+from src.vars import full_ver, chat_name, update_channel
 from src.colors import * 
+from init import update_channel as online_update_channel
 
-# Receive your global ip address for verification
-def get_global_ip():
-    response = requests.get('https://api.ipify.org?format=json')
-    data = response.json()
-    return data['ip']
+response = requests.get('https://api.strawberryfoundations.xyz/v1/versions')
+data = response.json()
 
-def check_for_updates():
-    response = requests.get('https://api.strawberryfoundations.xyz/v1/versions')
-    data = response.json()
+online_ver = data['stbchat']['server'][online_update_channel]
+
+def update():
+    print(f"{Colors.BOLD + GREEN}An update for {chat_name} is available.{RESET + Colors.RESET}\n")
+    print(f"{Colors.BOLD + CYAN}strawberry-chat{GREEN}@{MAGENTA + online_update_channel} {RESET}{online_ver}{Colors.RESET}")
+    print(f"↳ Upgrading from {CYAN + Colors.BOLD}strawberry-chat{GREEN}@{MAGENTA + update_channel} {RESET}{full_ver}{Colors.RESET}")
+
+def check_for_updates(subcommand_arg):
+    if subcommand_arg != "--force":
+        if online_ver == full_ver:
+            print(f"{Colors.BOLD + GREEN}You are on the latest version! ({online_ver}){RESET + Colors.RESET}")
+            
+        else:
+            update()
     
-    if data['strawberry-chat'] == short_ver:
-        print(f"{Colors.BOLD + GREEN}You are on the latest version! ({data['strawberry-chat']}){RESET + Colors.RESET}")
     else:
-        print(f"{Colors.BOLD + GREEN}An update is available.{RESET + Colors.RESET}")
-        print(f"{Colors.BOLD}strawberry-chat {short_ver} -> {data['strawberry-chat']}{RESET + Colors.RESET}")
-    
-    # return data['strawberry-chat']
+        update()
