61400fede132a642fe5b2e1ecd87124772d0f3c7
Juliandev02 <juli.mackenstein@gmail.com>
2023-09-30 17:02:42 +0200

    Updated versions

--- server/src/vars.py
+++ server/src/vars.py
@@ -2,7 +2,7 @@ from .colors import *
 
 # Version-specified Variables & important variables
 base_ver                = "1.8.0"
-short_ver               = "1.8.0b3+u6"
+short_ver               = "1.8.0b4+u1"
 ver                     = short_ver + "-vacakes"
 chat_name               = "Strawberry Chat"
 codename                = "Vanilla Cake"
