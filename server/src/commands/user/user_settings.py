84df60833e76a002ae4ef4823a0b54e51168a40a
Juliandev02 <juli.mackenstein@gmail.com>
2023-09-30 16:04:15 +0200

    Fixed some "bugs"

--- server/src/commands/user/user_settings.py
+++ server/src/commands/user/user_settings.py
@@ -8,18 +8,16 @@ from src.vars import user_settings_help, admin_settings_help
 
 from init import server_dir
 
+cmd_db = Database(server_dir + "/users.db", check_same_thread=False)
+
 @register_command("settings", arg_count=1)
 def user_settings_command(socket: socket.socket, username: str, args: list):
-    cmd_db = Database(server_dir + "/users.db", check_same_thread=False)
-    
     match args[0]:
         case "help":
             socket.send(f"{user_settings_help}{RESET}".encode("utf8"))
             
 @register_command("admin", arg_count=1)
-def user_settings_command(socket: socket.socket, username: str, args: list):
-    cmd_db = Database(server_dir + "/users.db", check_same_thread=False)
-    
+def admin_settings_command(socket: socket.socket, username: str, args: list):
     match args[0]:
         case "help":
             socket.send(f"{admin_settings_help}{RESET}".encode("utf8"))
