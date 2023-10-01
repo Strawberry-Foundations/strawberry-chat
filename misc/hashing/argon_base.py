a0d3621beae12450f716350d298f349a8907d605
Juliandev02 <juli.mackenstein@gmail.com>
2023-10-01 14:47:54 +0200

    Updated basic password hashing (misc/hashing)

--- misc/hashing/argon_base.py
+++ misc/hashing/argon_base.py
@@ -1,7 +1,8 @@
 import argon2
 
 def hash_password(password):
-    hashed_password = hash_password(password)
+    ph = argon2.PasswordHasher()
+    hashed_password = ph.hash(password)
     return hashed_password
 
 password = input("Password: ")
