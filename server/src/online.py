aebd78930e80df10e4811a1630094a4a5c75a50a
Juliandev02 <juli.mackenstein@gmail.com>
2023-10-01 12:14:13 +0200

    added input for asking to continue update process

--- server/src/online.py
+++ server/src/online.py
@@ -15,7 +15,15 @@ def update():
     
     print(f"{Colors.BOLD + GREEN}An update for {chat_name} is available.{RESET + Colors.RESET}\n")
     print(f"{Colors.BOLD + CYAN}strawberry-chat{GREEN}@{MAGENTA + online_update_channel} {RESET}{online_ver}{Colors.RESET}")
-    print(f"↳ Upgrading from {CYAN + Colors.BOLD}strawberry-chat{GREEN}@{MAGENTA + update_channel} {RESET}{full_ver}{Colors.RESET}")
+    print(f"↳ Upgrading from {CYAN + Colors.BOLD}strawberry-chat{GREEN}@{MAGENTA + update_channel} {RESET}{full_ver}{Colors.RESET}\n")
+    
+    w_cont = input(f"{Colors.BOLD}Do you want to continue? {CYAN}[Y/N]{RESET}: {GREEN}")
+    
+    if w_cont.lower() in ["y", "yes"]:
+        print(f"{RESET + Colors.RESET}→ Starting strawberry-chat update ...{Colors.RESET}")
+        
+    else:
+        print(f"{YELLOW}Cancelling update process...{RESET}")
 
 def check_for_updates(subcommand_arg):
     online_ver = fetch_update_data()
