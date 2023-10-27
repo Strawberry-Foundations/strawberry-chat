0e36762e604aecb84e68c0a1f7cd91a8a4567658
Juliandev02 <juli.mackenstein@gmail.com>
2023-10-27 23:53:47 +0200

    (Finally) Fixed #47 (No. 2)

--- server/src/commands/user/memberlist.py
+++ server/src/commands/user/memberlist.py
@@ -8,7 +8,7 @@ from src.db import Database
 
 from init import server_dir, users, config, online_mode, global_ip
 from src.vars import api
-from src.functions import memberListNickname, isOnline
+from src.functions import memberListNickname, memberListBadge, isOnline
 
 import yaml
 from yaml import SafeLoader
@@ -17,6 +17,8 @@ from yaml import SafeLoader
 @register_command("memberlist")
 @register_command("userlist")
 def memberlist_command(socket: socket.socket, username: str, args: list):
+    badge = {}
+    
     cmd_db = Database(server_dir + "/users.db", check_same_thread=False)
                 
     cmd_db.execute("SELECT username FROM users")
@@ -26,18 +28,18 @@ def memberlist_command(socket: socket.socket, username: str, args: list):
     cmd_db.execute("SELECT username, badge FROM users WHERE role = 'admin'")
     raw_admins  = cmd_db.fetchall()
     admins_len  = len([raw_admins for raw_admins in sorted(raw_admins)])
-    admins      = "\n           ".join([f"{isOnline(result[0])}{LIGHTRED_EX} {memberListNickname(result[0])} [{result[1]}]" for result in raw_admins])
+    admins      = "\n           ".join([f"{isOnline(result[0])}{LIGHTRED_EX} {memberListNickname(result[0])} {memberListBadge(result[0])}" for result in raw_admins])
     
     cmd_db.execute("SELECT username, badge FROM users WHERE role = 'bot'")
     raw_bots    = cmd_db.fetchall()
     bots_len    = len([raw_bots for raw_bots in sorted(raw_bots)])
-    bots      = "\n           ".join([f"{isOnline(result[0])}{LIGHTMAGENTA_EX} {memberListNickname(result[0])} [{result[1]}]" for result in raw_bots])
+    bots        = "\n           ".join([f"{isOnline(result[0])}{LIGHTMAGENTA_EX} {memberListNickname(result[0])} {memberListBadge(result[0])}" for result in raw_bots])
     
     
     cmd_db.execute("SELECT username, badge FROM users WHERE role = 'member'")
     raw_members = cmd_db.fetchall()
     members_len = len([raw_members for raw_members in sorted(raw_members)])
-    members      = "\n           ".join([f"{isOnline(result[0])}{LIGHTYELLOW_EX} {memberListNickname(result[0])} [{result[1]}]" for result in raw_members])
+    members     = "\n           ".join([f"{isOnline(result[0])}{LIGHTYELLOW_EX} {memberListNickname(result[0])} {memberListBadge(result[0])}" for result in raw_members])
     
     try:
         if online_mode == True:
