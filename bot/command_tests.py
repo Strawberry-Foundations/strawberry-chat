fbefd19f6e9cb2abcc1c927a82b9c3344c458f7b
Juliandev02 <juli.mackenstein@gmail.com>
2023-10-28 00:07:32 +0200

    Fully removed command function from command_tests bot

--- bot/command_tests.py
+++ bot/command_tests.py
@@ -26,28 +26,6 @@ def test_command(username: str, args: list):
         Args: {args}"""
         )
 
-def Commands():
-    while True:
-        try:
-            recv_message = Bot.recv_message(raw=False, ansi=False)
-            index       = recv_message.find(":")
-            raw_message = recv_message[index + 2:]
-            
-            if raw_message.startswith("!"):
-                message = raw_message[1:]
-                args = message.split()
-                cmd = args[0]
-                args = args[1:]
-                
-                Bot.execute_command(cmd, Bot.get_username_by_msg(recv_message), args)
-                continue
-
-        except Exception as e: 
-            Bot.logger(f"{scapi.RED}An unknown exception occured{scapi.RESET}", type=Scapi.LogLevel.ERROR)
-            Bot.logger(f"{scapi.RED}{e}{scapi.RESET}", type=Scapi.LogLevel.ERROR)
-            break
-
-
 @Bot.event
 def on_ready():
     print(f"{Bot.log_msg}{scapi.BLUE}{Bot.username} started successfully!{scapi.RESET}")
