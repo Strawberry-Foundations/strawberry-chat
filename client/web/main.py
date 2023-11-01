a18584e17c0ff48998ad3618bb5ea29cd36c57eb
Juliandev02 <juli.mackenstein@gmail.com>
2023-11-01 14:10:43 +0100

    Updated web client

--- client/web/main.py
+++ client/web/main.py
@@ -17,12 +17,16 @@ def currentTime():
 def index():
     return "Currently nothing here >->"
 
-@app.route('/chat')
+@app.route('/chat', methods=['GET', 'POST'])
 def chat():
     global uname
     global password
     uname = request.args.get('uname')
     password = request.args.get('password')
+    
+    if request.method == "POST":
+        message = request.form['message']
+        clientSocket.send(message.encode("utf8"))
     
     return render_template('chat.html')
 
@@ -31,6 +35,10 @@ def escape_ansi(line):
     return ansi_escape.sub('', line)
 
 def recv():
+    clientSocket.send("jules".encode("utf8"))
+    time.sleep(0.1)
+    clientSocket.send("test".encode("utf8"))
+    
     while threadFlag:
         try:
             message = clientSocket.recv(2048).decode()
@@ -70,7 +78,7 @@ def main():
     sendingThread = threading.Thread(target=send, args=(clientSocket,))
     sendingThread.start()
     
-    app.run()
+    app.run(host="0.0.0.0")
     
 threadFlag = True
 
