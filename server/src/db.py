ada70d4c8622beb79e94d647d287e190533a9363
Juliandev02 <juli.mackenstein@gmail.com>
2023-11-10 16:08:21 +0100

    Readded db wrapper because it is a dependency for many commands

---
+++ server/src/db.py
@@ -0,0 +1,28 @@
+import sqlite3 as sql
+import os 
+import sys
+from init import * 
+
+
+class Database:
+    def __init__(self, database, check_same_thread: bool = False):
+        self.db_connection = sql.connect(database, check_same_thread)
+        self.cursor = self.db_connection.cursor()
+
+    def execute(self, statement, __parameters = ()):
+        self.cursor.execute(statement, __parameters)
+
+    def commit(self):
+        self.db_connection.commit()
+
+    def close(self):
+        self.cursor.close()
+
+    def cursor_close(self):
+        self.db_connection.close()
+
+    def fetchone(self):
+        return self.cursor.fetchone()
+
+    def fetchall(self):
+        return self.cursor.fetchall()
