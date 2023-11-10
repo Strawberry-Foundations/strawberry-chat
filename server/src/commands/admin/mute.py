170447a69fd2f1ec0715b3033310e8c60183ea34
Juliandev02 <juli.mackenstein@gmail.com>
2023-11-10 22:09:06 +0100

    Migrated unmute command to mute sources

--- server/src/commands/admin/mute.py
+++ server/src/commands/admin/mute.py
@@ -22,3 +22,18 @@ def mute_command(socket: socket.socket, username: str, args: list):
         cmd_db.commit()
 
         socket.send(f"{LIGHTGREEN_EX + Colors.BOLD}Muted {uname}{RESET + Colors.RESET}".encode("utf8"))
+
+@register_command("unmute", arg_count=1, required_permissions=PermissionLevel.ADMIN)
+def unmute_command(socket: socket.socket, username: str, args: list):
+    cmd_db = Database(server_dir + "/users.db", check_same_thread=False)
+
+    uname = args[0]
+
+    if doesUserExist(uname) == False:
+        socket.send(f"{RED + Colors.BOLD}Sorry, this user does not exist!{RESET + Colors.RESET}".encode("utf8"))
+    
+    else:
+        cmd_db.execute("UPDATE users SET muted = 'false' WHERE username = ?", (uname,))
+        cmd_db.commit()
+
+        socket.send(f"{LIGHTGREEN_EX + Colors.BOLD}Unmuted {uname}{RESET + Colors.RESET}".encode("utf8"))
