50084c5a0b526e28bdbf9d2ffcb55119a2f2c0c4
Juliandev02 <juli.mackenstein@gmail.com>
2023-11-10 23:22:17 +0100

    Moved logging to init.py

--- server/init.py
+++ server/init.py
@@ -1,28 +1,72 @@
 import os
 import yaml
 import requests
+import logging
+import datetime
 
 from enum import Enum
 from yaml import SafeLoader
+from colorama import Style
 
-from src.colors import YELLOW, Colors, RESET, GREEN, CYAN
+from src.colors import *
 from src.vars import chat_name, short_ver, codename, server_edition
+
+# Path of init.py
+server_dir = os.path.dirname(os.path.realpath(__file__))
 
 class StbTypes(Enum):
     INFO = 0
     WARNING = 2
     ERROR = 1
 
-# Startup title
-print(f"{CYAN + Colors.BOLD}* -- {chat_name} v{short_ver} {codename} ({server_edition}) -- *{RESET + Colors.RESET}")
-
-# Path of init.py
-server_dir = os.path.dirname(os.path.realpath(__file__))
-
 afks = list([])
 users = {}
 addresses = {}
 user_logged_in = {}
+
+# Init logger
+class LogFormatter(logging.Formatter):
+    class Fmt:
+        info     = f"{RESET}[{datetime.datetime.now().strftime('%H:%M')}] {BLUE}[%(levelname)s]{RESET + Colors.RESET}    %(message)s"
+        error    = f"{RESET}[{datetime.datetime.now().strftime('%H:%M')}] {RED}[%(levelname)s]{RESET + Colors.BOLD}   %(message)s"
+        default  = f"{RESET}[{datetime.datetime.now().strftime('%H:%M')}] {BLUE}[%(levelname)s]{RESET + Colors.RESET + Colors.BOLD}   %(message)s"
+        warning  = f"{RESET}[{datetime.datetime.now().strftime('%H:%M')}] {YELLOW}[%(levelname)s]{RESET + Colors.RESET + Colors.BOLD} %(message)s"
+        critical = f"{RESET}[{datetime.datetime.now().strftime('%H:%M')}] {RED}[%(levelname)s]{RESET + Colors.RESET + Colors.BOLD} %(message)s"
+
+    FORMATS = {
+        logging.DEBUG:    Colors.BOLD + Fmt.default,
+        logging.INFO:     Colors.BOLD + Fmt.info,
+        logging.WARNING:  Colors.BOLD + Fmt.warning,
+        logging.ERROR:    Colors.BOLD + Fmt.error,
+        logging.CRITICAL: Colors.BOLD + Fmt.critical
+    }
+
+    def format(self, record):
+        log_fmt = Style.RESET_ALL + self.FORMATS.get(record.levelno) + Style.RESET_ALL
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
+log = logging.getLogger("LOG")
+
+if os.environ.get("LOG_LEVEL") is not None:
+    log.setLevel(os.environ.get("LOG_LEVEL").upper())
+    
+else:
+    log.setLevel("INFO")
+    
+log_fh  = logging.FileHandler(server_dir + '/log.txt')
+log_fmt = logging.Formatter(f"(%(asctime)s) [%(levelname)s]  %(message)s")
+log_fh.setFormatter(log_fmt)
+
+log_ch = logging.StreamHandler()
+log_ch.setFormatter(LogFormatter())
+
+log.addHandler(log_ch)
+log.addHandler(log_fh)
+
+# Startup title
+print(f"{CYAN + Colors.BOLD}* -- {chat_name} v{short_ver} {codename} ({server_edition}) -- *{RESET + Colors.RESET}")
 
 # Open Configuration
 with open(server_dir + "/config.yml") as config_data:
