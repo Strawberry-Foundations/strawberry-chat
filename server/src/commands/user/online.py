3890d535f9b419a2d6f9f7a4805f21bc3bcee65b
Juliandev02 <juli.mackenstein@gmail.com>
2023-11-11 20:35:50 +0100

    Fixed online command (added send argument)

--- server/src/commands/user/online.py
+++ server/src/commands/user/online.py
@@ -7,7 +7,7 @@ from src.colors import *
 from init import users
 
 @register_command("online")
-def online_command(socket: socket.socket, username: str, args: list):
+def online_command(socket: socket.socket, username: str, args: list, send):
     onlineUsers = ', '.join([user for user in sorted(users.values())])
     onlineUsersLen2 = len([user for user in sorted(users.values())])
     socket.send(f"""{GREEN +  Colors.UNDERLINE + Colors.BOLD}Users who are currently online ({onlineUsersLen2}){RESET + Colors.RESET}
