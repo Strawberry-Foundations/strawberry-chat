0d5901d0947080538d7eae00df1e9605aad89187
Juliandev02 <juli.mackenstein@gmail.com>
2023-11-11 22:30:29 +0100

    Updated exit_cmd (added log.info)

--- server/src/commands/default/exit_cmd.py
+++ server/src/commands/default/exit_cmd.py
@@ -5,7 +5,7 @@ import yaml
 
 from src.colors import *
 from yaml import SafeLoader
-from init import server_dir, addresses, users, user_logged_in
+from init import server_dir, addresses, users, user_logged_in, log
 from src.functions import broadcast_all, userRoleColor
 
 # Open Configuration
@@ -16,11 +16,14 @@ with open(server_dir + "/config.yml") as config_data:
 @register_command("quit")
 def exit_command(socket: socket.socket, username: str, args: list, send):
     send(f"{YELLOW + Colors.BOLD}You left the chat!{RESET + Colors.RESET}")
+    log.info(f"{username} {addresses[socket][0]} has left.")
+    
     del addresses[socket]
     del users[socket]
     socket.close()
     
+    
     user_logged_in[username] = False
     
-    # log.info(f"{user} ({address}) has left.")
+    
     broadcast_all(f"{Colors.GRAY + Colors.BOLD}<--{Colors.RESET} {userRoleColor(username)}{username}{YELLOW + Colors.BOLD} has left the chat room!{RESET + Colors.RESET}")
