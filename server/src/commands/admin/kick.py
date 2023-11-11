bc63f59eef4ee6d879237dd16997b8e413856615
Juliandev02 <juli.mackenstein@gmail.com>
2023-11-11 20:34:11 +0100

    Fixed kick command (added send argument)

--- server/src/commands/admin/kick.py
+++ server/src/commands/admin/kick.py
@@ -9,7 +9,7 @@ from src.db import Database
 from init import server_dir, users, addresses, log
 
 @register_command("kick", arg_count=1, required_permissions=PermissionLevel.ADMIN)
-def kick_command(socket: socket.socket, username: str, args: list):
+def kick_command(socket: socket.socket, username: str, args: list, send):
     cmd_db  = Database(server_dir + "/users.db", check_same_thread=False)
     
     uname   = args[0]
