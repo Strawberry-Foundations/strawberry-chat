8b32984e1ca2896b6523c29d4c6ae7704baad54d
Juliandev02 <juli.mackenstein@gmail.com>
2023-11-12 00:55:55 +0100

    Forgot to add the 2 after the a oops

--- server/src/vars.py
+++ server/src/vars.py
@@ -2,7 +2,7 @@ from .colors import *
 
 # Version-specified Variables & important variables
 base_ver                = "1.8.1"
-short_ver               = "1.9.0a"
+short_ver               = "1.9.0a2"
 full_ver                = "v" + short_ver
 update_channel          = "canary"
 ver                     = short_ver + "-vacakes"
