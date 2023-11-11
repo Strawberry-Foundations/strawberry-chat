d97eab495b0d6ebf3aa31d35421ef8cfc81f0674
Juliandev02 <juli.mackenstein@gmail.com>
2023-11-11 21:16:57 +0100

    Updated replace.py

--- misc/tools/replace.py
+++ misc/tools/replace.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 import sys
+import os
+
+def replace_keywords_in_folder(folder_path, replacements):
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        if os.path.isfile(file_path):
+            replace_keywords(file_path, replacements)
 
 def replace_keywords(file_path, replacements):
     try:
@@ -19,10 +26,12 @@ def replace_keywords(file_path, replacements):
 
 if __name__ == "__main__":
     input_filename = sys.argv[1]
+    folder_path = sys.argv[1]
     
     keyword_replacements = {
         'socket.send': 'send',
         '.encode("utf8")': '',
     }
 
-    replace_keywords(input_filename, keyword_replacements)
+    replace_keywords_in_folder(folder_path, keyword_replacements)
+    # replace_keywords(input_filename, keyword_replacements)
