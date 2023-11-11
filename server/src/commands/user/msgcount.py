289a95ee0aaf098536b5fd7aacbdca49ad3bfe31
Juliandev02 <juli.mackenstein@gmail.com>
2023-11-11 20:35:44 +0100

    Fixed msgcount command (added send argument)

--- server/src/commands/user/msgcount.py
+++ server/src/commands/user/msgcount.py
@@ -8,7 +8,7 @@ from src.db import Database
 from init import server_dir
 
 @register_command("msgcount")
-def msgcount_command(socket: socket.socket, username: str, args: list):
+def msgcount_command(socket: socket.socket, username: str, args: list, send):
     cmd_db = Database(server_dir + "/users.db", check_same_thread=False)
     cmd_db.execute("SELECT msg_count FROM users WHERE username = ?", (username,))
     msg_count = cmd_db.fetchone()
