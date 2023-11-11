d270cac47e9a58ad06c9ef364cb4ed85993b710d
Juliandev02 <juli.mackenstein@gmail.com>
2023-11-11 20:35:05 +0100

    Fixed afklist command (added send argument)

--- server/src/commands/user/afklist.py
+++ server/src/commands/user/afklist.py
@@ -8,7 +8,7 @@ from init import afks
 
 @register_command("afks")
 @register_command("afklist")
-def afklist_command(socket: socket.socket, username: str, args: list):
+def afklist_command(socket: socket.socket, username: str, args: list, send):
     afkUsers = ', '.join([afks for afks in sorted(afks)])
     afkUsersLen = len([afks for afks in sorted(afks)])
     socket.send(f"""{GREEN +  Colors.UNDERLINE + Colors.BOLD}Users who are currently Afk ({afkUsersLen}){RESET + Colors.RESET}
