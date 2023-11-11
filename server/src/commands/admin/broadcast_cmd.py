a5f152f4d1fe225bc39bceb68b6fb3f4738cfc1d
Juliandev02 <juli.mackenstein@gmail.com>
2023-11-11 20:33:58 +0100

    Fixed broadcast command (added send argument)

--- server/src/commands/admin/broadcast_cmd.py
+++ server/src/commands/admin/broadcast_cmd.py
@@ -7,7 +7,7 @@ from src.functions import repl_htpf, broadcast_all
 
 @register_command("broadcast", arg_count=1, required_permissions=PermissionLevel.ADMIN)
 @register_command("rawsay", arg_count=1, required_permissions=PermissionLevel.ADMIN)
-def broadcast_command(socket: socket.socket, username: str, args: list):
+def broadcast_command(socket: socket.socket, username: str, args: list, send):
     text = " ".join(args)
     text = repl_htpf(text)
     broadcast_all(f"{text}{RESET}")
