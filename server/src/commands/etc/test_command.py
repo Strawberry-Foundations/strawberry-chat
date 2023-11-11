9d65153fc9f23347962e055206bc0fcd632b6b46
Juliandev02 <juli.mackenstein@gmail.com>
2023-11-11 20:34:56 +0100

    Fixed test_command command (added send argument)

--- server/src/commands/etc/test_command.py
+++ server/src/commands/etc/test_command.py
@@ -3,6 +3,6 @@ from .. import register_command
 
 
 @register_command("test", 2)
-def test_command(socket: socket.socket, username: str, args: list):
+def test_command(socket: socket.socket, username: str, args: list, send):
     socket.send(f"Username: {username}".encode("utf8"))
     socket.send(f"Args: {args}".encode("utf8"))
