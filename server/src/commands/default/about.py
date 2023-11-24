28c41365b0d0acae222d41245cbcd711fe181fdf
Juliandev02 <juli.mackenstein@gmail.com>
2023-11-24 19:41:40 +0100

    Added ext_ver to about command

--- server/src/commands/default/about.py
+++ server/src/commands/default/about.py
@@ -3,7 +3,7 @@ from .. import register_command
 import socket
 import yaml
 
-from src.vars import chat_name, short_ver, codename, server_edition, authors
+from src.vars import chat_name, short_ver, codename, server_edition, authors, ext_ver
 from src.colors import *
 from yaml import SafeLoader
 from init import server_dir
@@ -16,5 +16,5 @@ with open(server_dir + "/config.yml") as config_data:
 def about_command(socket: socket.socket, username: str, args: list, send):
     send(f"""{GREEN +  Colors.UNDERLINE + Colors.BOLD}About {chat_name}{RESET + Colors.RESET}
         {BLUE + Colors.BOLD}Thank you for using {chat_name}!{RESET}
-        {BLUE + Colors.BOLD}Version: {RESET}{short_ver} {codename} ({server_edition})
+        {BLUE + Colors.BOLD}Version: {RESET}{short_ver} {codename} ({server_edition}) ({ext_ver})
         {BLUE + Colors.BOLD}Author: {RESET}{", ".join(authors)}{RESET + Colors.RESET}""")
