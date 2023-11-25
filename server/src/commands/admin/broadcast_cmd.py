c523e33dcff5f440894bbe101676931fa2bc23cb
Juliandev02 <juli.mackenstein@gmail.com>
2023-11-25 17:51:10 +0100

    renamed repl_htpf to replace_htpf for better readability

--- server/src/commands/admin/broadcast_cmd.py
+++ server/src/commands/admin/broadcast_cmd.py
@@ -3,11 +3,11 @@ from .. import register_command, PermissionLevel
 import socket
 
 from src.colors import *
-from src.functions import repl_htpf, broadcast_all
+from src.functions import replace_htpf, broadcast_all
 
 @register_command("broadcast", arg_count=1, required_permissions=PermissionLevel.ADMIN)
 @register_command("rawsay", arg_count=1, required_permissions=PermissionLevel.ADMIN)
 def broadcast_command(socket: socket.socket, username: str, args: list, send):
     text = " ".join(args)
-    text = repl_htpf(text)
+    text = replace_htpf(text)
     broadcast_all(f"{text}{RESET}")
