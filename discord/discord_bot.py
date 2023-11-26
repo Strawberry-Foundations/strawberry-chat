f35b1d25d6d9ca346981a417adf2f44ac03ca4ac
Juliandev02 <juli.mackenstein@gmail.com>
2023-11-26 14:10:28 +0100

    added json flag

--- discord/discord_bot.py
+++ discord/discord_bot.py
@@ -26,7 +26,7 @@ stbchat_host    = config["stbchat"]["server"]["host"]
 stbchat_port    = config["stbchat"]["server"]["port"]
 
 bot = commands.Bot(command_prefix='.', intents=discord.Intents.all())
-scapi_bot = Scapi.Bot(username=stbchat_uname, token=stbchat_token, host=stbchat_host, port=stbchat_port)
+scapi_bot = Scapi.Bot(username=stbchat_uname, token=stbchat_token, host=stbchat_host, port=stbchat_port, json=True)
 
 scapi_bot.login()
 scapi_bot.flag_handler(print_recv_msg=False, enable_user_input=True)
