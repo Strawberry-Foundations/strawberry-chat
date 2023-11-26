d8311426a48b67ff379f1a7efeb87cf88b2e4b04
Juliandev02 <juli.mackenstein@gmail.com>
2023-11-26 16:55:15 +0100

    some improvements to queue cmd

--- server/src/commands/admin/queue_cmd.py
+++ server/src/commands/admin/queue_cmd.py
@@ -16,9 +16,38 @@ def queue_command(socket: socket.socket, username: str, args: list, send):
     
     match args[0]:
         case "remove":
-            if not len(queue.queue) >= 2:
-                send(f"{RED}Cannot remove first position in queue: Only one person is currently in queue")
-                return
+            if len(args) == 1:
+                if not len(queue.queue) >= 2:
+                    send(f"{RED}Cannot remove first position in queue: Only one or none person is currently in queue")
+                    return
+                
+                
+                log.info(f"{queue.queue[(position - 1)]} got kicked out of the queue")
+                queue.remove()
+                send(f"{GREEN}The first position in the queue has been removed")
+                
             
-            queue.remove()
-            send(f"{GREEN}The first position in the queue has been removed")
+            else: 
+                position = int(args[1])
+                
+                try:
+                    log.info(f"{queue.queue[(position - 1)]} got kicked out of the queue")
+                    queue.remove(pos=(position - 1))
+                    send(f"{GREEN}Removed position {position} from the queue")
+                except: 
+                    send(f"{RED}This position is not available")
+                    return
+                
+                
+        
+        case "list":
+            queue_list = ""
+            
+            if len(queue.queue) == 0:
+                send(f"{RED}Queue is empty")
+                return
+                
+            for pos, user in enumerate(queue.queue, start=1):
+                queue_list = queue_list + f"\n#{pos}: {user}"
+                
+            send(f"{queue_list}")
