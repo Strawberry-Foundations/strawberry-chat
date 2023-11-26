bead8417a6a1eb0a034758608b8e0f2141952e5f
Juliandev02 <juli.mackenstein@gmail.com>
2023-11-26 17:30:58 +0100

    removed unnessecary new line

--- server/src/commands/user/online.py
+++ server/src/commands/user/online.py
@@ -13,7 +13,6 @@ def online_command(socket: socket.socket, username: str, args: list, send):
     
     _online_users = f"({onlineUsersLen2}/{max_users})"
     if max_users == -1: _online_users = f"({onlineUsersLen2})"
-        
     
     send(f"""{GREEN +  Colors.UNDERLINE + Colors.BOLD}Users who are currently online {_online_users}{RESET + Colors.RESET}
         {Colors.BOLD}->{Colors.RESET} {CYAN}{onlineUsers}{RESET + Colors.RESET}""")
