96d35136769bfecd4d00c8e4c11dbc1680f58b32
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-01 19:41:45 +0100

    readded old database wrapper

--- server/src/db.py
+++ server/src/db.py
@@ -40,3 +40,26 @@ class SQLiteDatabaseWrapper:
     def rollback(self):
         if self.connection:
             self.connection.rollback()
+
+class Database:
+    def __init__(self, database, check_same_thread: bool = False):
+        self.db_connection = sql.connect(database, check_same_thread)
+        self.cursor = self.db_connection.cursor()
+
+    def execute(self, statement, __parameters = ()):
+        self.cursor.execute(statement, __parameters)
+
+    def commit(self):
+        self.db_connection.commit()
+
+    def close(self):
+        self.cursor.close()
+
+    def cursor_close(self):
+        self.db_connection.close()
+
+    def fetchone(self):
+        return self.cursor.fetchone()
+
+    def fetchall(self):
+        return self.cursor.fetchall()
