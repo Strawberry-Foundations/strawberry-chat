52464d446c76407c0a7913437d641a2622fbf481
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-08 23:02:34 +0100

    Updated versions

--- server/src/vars.py
+++ server/src/vars.py
@@ -2,7 +2,7 @@ from .colors import *
 
 # Version-specified Variables & important variables
 base_ver                = "1.10.0"
-short_ver               = "1.10.0a1"
+short_ver               = "1.10.0a2"
 full_ver                = "v" + short_ver
 stbm_ver                = "v2"
 ver                     = short_ver + "-vacakes"
