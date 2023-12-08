0b83968a5c869532cf8cf4c1e0699cbcbb80897e
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-08 20:52:39 +0100

    more unsuccessful tests :(

--- server/database_test.py
+++ server/database_test.py
@@ -1,5 +1,5 @@
 from init import *
 
 db = Database(driver="mysql", database_path="./users.db")
-# print(db.fetch_all("SELECT username FROM users WHERE username = ?", ("Julian",)))
+print(db.fetch_all("SELECT password FROM users WHERE username = ?", ("Julian",)))
 db._execute_query('INSERT INTO users (username, password, role, role_color, enable_blacklisted_words, account_enabled, muted, user_id, msg_count, enable_dms, creation_date) VALUES ("Julian", "test", "member", "red", "true", "true", "false", 0, 0, true, 1);')
