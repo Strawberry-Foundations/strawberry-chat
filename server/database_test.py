a040f34cca8ee325a35d571eebd678a7b1a472ea
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-08 20:49:19 +0100

    more unsuccessful tests :(

--- server/database_test.py
+++ server/database_test.py
@@ -1,4 +1,5 @@
 from init import *
 
-db = Database(driver="sqlite", database_path="./users.db")
-print(db.fetch_all("SELECT nickname FROM users WHERE username = ?", ("Julian",)))
+db = Database(driver="mysql", database_path="./users.db")
+# print(db.fetch_all("SELECT username FROM users WHERE username = ?", ("Julian",)))
+db._execute_query('INSERT INTO users (username, password, role, role_color, enable_blacklisted_words, account_enabled, muted, user_id, msg_count, enable_dms, creation_date) VALUES ("Julian", "test", "member", "red", "true", "true", "false", 0, 0, true, 1);')
