ba37d6e368a3a00961357be1859d125cd1d26449
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-09 13:44:25 +0100

    Fixed debug command (user & sender property)

--- server/src/commands/admin/debug.py
+++ server/src/commands/admin/debug.py
@@ -5,20 +5,20 @@ import time
 
 from src.colors import *
 from src.functions import replace_htpf, broadcast_all
-from init import addresses, users
+from init import User, ClientSender, addresses, users
 
 @register_command("debug", arg_count=0, required_permissions=PermissionLevel.ADMIN)
-def debug_command(socket: socket.socket, username: str, args: list, send):
-    send(f"""Client Object: {socket}
+def debug_command(socket: socket.socket, user: User, args: list, sender: ClientSender):
+    sender.send(f"""Client Object: {socket}
         IP Object: {addresses[socket]}
         User Object: {users[socket]}
         Addresses: {addresses}
         Users: {users}""")
     
 @register_command("clientinfo", arg_count=0, required_permissions=PermissionLevel.MEMBER)
-def clientinfo_command(socket: socket.socket, username: str, args: list, send):
-    send(f"{addresses[socket]}")
+def clientinfo_command(socket: socket.socket, user: User, args: list, sender: ClientSender):
+    sender.send(f"{addresses[socket]}")
     time.sleep(0.1)
-    send(f"{users[socket]}")
+    sender.send(f"{users[socket]}")
     time.sleep(0.1)
-    send(f"{socket}")
+    sender.send(f"{socket}")
