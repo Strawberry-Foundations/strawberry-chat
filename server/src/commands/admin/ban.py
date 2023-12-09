f935c212fd47faeb5d4ecfc9c7f32f97b69860e9
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-09 13:41:33 +0100

    Fixed ban & unban command (user & sender property)

--- server/src/commands/admin/ban.py
+++ server/src/commands/admin/ban.py
@@ -6,36 +6,36 @@ from src.colors import *
 from src.functions import doesUserExist
 from src.db import Database
 
-from init import server_dir, log
+from init import User, ClientSender, server_dir, log
 
 @register_command("ban", arg_count=1, required_permissions=PermissionLevel.ADMIN)
-def ban_command(socket: socket.socket, username: str, args: list, send):
+def ban_command(socket: socket.socket, user: User, args: list, sender: ClientSender):
     cmd_db = Database(server_dir + "/users.db", check_same_thread=False)
 
     uname = args[0]
 
     if doesUserExist(uname) == False:
-        send(f"{RED + Colors.BOLD}Sorry, this user does not exist!{RESET + Colors.RESET}")
+        sender.send(f"{RED + Colors.BOLD}Sorry, this user does not exist!{RESET + Colors.RESET}")
     
     else:
         cmd_db.execute("UPDATE users SET account_enabled = 'false' WHERE username = ?", (uname,))
         cmd_db.commit()
 
-        send(f"{LIGHTGREEN_EX + Colors.BOLD}Banned {uname}{RESET + Colors.RESET}")
-        log.info(f"{uname} has been banned by {username}")
+        sender.send(f"{LIGHTGREEN_EX + Colors.BOLD}Banned {uname}{RESET + Colors.RESET}")
+        log.info(f"{uname} has been banned by {user.username}")
 
 @register_command("unban", arg_count=1, required_permissions=PermissionLevel.ADMIN)
-def unban_command(socket: socket.socket, username: str, args: list, send):
+def unban_command(socket: socket.socket, user: User, args: list, sender: ClientSender):
     cmd_db = Database(server_dir + "/users.db", check_same_thread=False)
 
     uname = args[0]
 
     if doesUserExist(uname) == False:
-        send(f"{RED + Colors.BOLD}Sorry, this user does not exist!{RESET + Colors.RESET}")
+        sender.send(f"{RED + Colors.BOLD}Sorry, this user does not exist!{RESET + Colors.RESET}")
     
     else:
         cmd_db.execute("UPDATE users SET account_enabled = 'true' WHERE username = ?", (uname,))
         cmd_db.commit()
 
-        send(f"{LIGHTGREEN_EX + Colors.BOLD}Unbanned {uname}{RESET + Colors.RESET}")
-        log.info(f"{uname} has been unbanned by {username}")
+        sender.send(f"{LIGHTGREEN_EX + Colors.BOLD}Unbanned {uname}{RESET + Colors.RESET}")
+        log.info(f"{uname} has been unbanned by {user.username}")
