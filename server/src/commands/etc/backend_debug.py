4c7ae88253d6ad02fdae40b7700f8ccaa9b4154c
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-09 23:51:10 +0100

    Fixed command name

--- server/src/commands/etc/backend_debug.py
+++ server/src/commands/etc/backend_debug.py
@@ -6,7 +6,7 @@ from src.colors import *
 from src.functions import send_json
 from init import User, ClientSender
 
-@register_command("debug", arg_count=0, required_permissions=PermissionLevel.ADMIN)
+@register_command("backend_debug", arg_count=0, required_permissions=PermissionLevel.ADMIN)
 def backend_debug_command(socket: socket.socket, user: User, args: list, sender: ClientSender):
     backend_builder = {
                         "message_type": "stbchat_backend",
