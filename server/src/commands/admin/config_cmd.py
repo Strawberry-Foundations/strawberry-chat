264252e03549fdd9917ccd6f9a1ec9c817f75e84
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-09 13:43:47 +0100

    Fixed config command (user & sender property)

--- server/src/commands/admin/config_cmd.py
+++ server/src/commands/admin/config_cmd.py
@@ -7,7 +7,8 @@ from src.db import Database
 from src.vars import server_settings_help
 from src.functions import hash_password, verify_password, escape_ansi
 
-from init import server_dir, ipaddr, port, \
+from init import User, ClientSender, \
+                 server_dir, ipaddr, port, \
                  enable_messages, enable_queue, \
                  max_message_length, max_users, \
                  debug_mode, online_mode, update_channel, \
@@ -15,17 +16,17 @@ from init import server_dir, ipaddr, port, \
 
 
 @register_command("config", arg_count=1, required_permissions=PermissionLevel.ADMIN)
-def user_settings_command(socket: socket.socket, username: str, args: list, send):
+def user_settings_command(socket: socket.socket, user: User, args: list, sender: ClientSender):
     cmd_db = Database(server_dir + "/users.db", check_same_thread=False)
     
     try:
         match args[0]:
             case "help":
-                send(f"{server_settings_help}{RESET}")
+                sender.send(f"{server_settings_help}{RESET}")
             
             case _:
-                send(f"{Colors.RESET + RED}Invalid subcommand!{RESET + Colors.RESET}")
+                sender.send(f"{Colors.RESET + RED}Invalid subcommand!{RESET + Colors.RESET}")
                     
     except Exception as e: 
-        send(f"{RED}Not enough arguments!{RESET}")
+        sender.send(f"{RED}Not enough arguments!{RESET}")
         print(e)
