65e150969719a42b4ace49d2333fade4885d6ee1
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-09 13:46:35 +0100

    Fixed role command (user & sender property)

--- server/src/commands/admin/role.py
+++ server/src/commands/admin/role.py
@@ -6,10 +6,10 @@ from src.colors import *
 from src.db import Database
 from src.vars import role_colors
 
-from init import server_dir
+from init import User, ClientSender, server_dir
 
 @register_command("role", arg_count=1, required_permissions=PermissionLevel.ADMIN)
-def role_command(socket: socket.socket, username: str, args: list, send):
+def role_command(socket: socket.socket, user: User, args: list, sender: ClientSender):
     cmd_db = Database(server_dir + "/users.db", check_same_thread=False)
 
     cmd = args[0]
@@ -21,16 +21,16 @@ def role_command(socket: socket.socket, username: str, args: list, send):
             role = args[2]
             
             if role not in ["member", "bot", "admin"]:
-                send(f"{RED + Colors.BOLD}Invalid role!{RESET + Colors.RESET}")
+                sender.send(f"{RED + Colors.BOLD}Invalid role!{RESET + Colors.RESET}")
                 return
         
             cmd_db.execute("UPDATE users SET role = ? WHERE username = ?", (role, uname))
             cmd_db.commit()
             
-            send(f"{LIGHTGREEN_EX + Colors.BOLD}Role of {uname} was set to {role}{RESET + Colors.RESET}")
+            sender.send(f"{LIGHTGREEN_EX + Colors.BOLD}Role of {uname} was set to {role}{RESET + Colors.RESET}")
         
         except:
-            send(f"{RED + Colors.BOLD}Invalid username and/or role!{RESET + Colors.RESET}")
+            sender.send(f"{RED + Colors.BOLD}Invalid username and/or role!{RESET + Colors.RESET}")
     
     # /role get Command
     elif cmd == "get":
@@ -40,10 +40,10 @@ def role_command(socket: socket.socket, username: str, args: list, send):
             cmd_db.execute("SELECT role FROM users WHERE username = ?", (uname,))
             role = cmd_db.fetchone()[0]
             
-            send(f"{LIGHTGREEN_EX + Colors.BOLD}Role of {uname}: {MAGENTA}{role}{RESET + Colors.RESET}")
+            sender.send(f"{LIGHTGREEN_EX + Colors.BOLD}Role of {uname}: {MAGENTA}{role}{RESET + Colors.RESET}")
         
         except:
-            send(f"{RED + Colors.BOLD}Invalid username!{RESET + Colors.RESET}")
+            sender.send(f"{RED + Colors.BOLD}Invalid username!{RESET + Colors.RESET}")
     
     # /role color Command
     elif cmd == "color":
@@ -52,16 +52,16 @@ def role_command(socket: socket.socket, username: str, args: list, send):
             color = args[2]
             
             if color.lower() not in role_colors:
-                send(f"{RED + Colors.BOLD}Invalid color!{RESET + Colors.RESET}")
+                sender.send(f"{RED + Colors.BOLD}Invalid color!{RESET + Colors.RESET}")
                 return
         
             cmd_db.execute("UPDATE users SET role_color = ? WHERE username = ?", (color.lower(), uname))
             cmd_db.commit()
             
-            send(f"{LIGHTGREEN_EX + Colors.BOLD}Role Color of {uname} was set to {color}{RESET + Colors.RESET}")
+            sender.send(f"{LIGHTGREEN_EX + Colors.BOLD}Role Color of {uname} was set to {color}{RESET + Colors.RESET}")
     
         except:
-            send(f"{RED + Colors.BOLD}Invalid username and/or color!{RESET + Colors.RESET}")
+            sender.send(f"{RED + Colors.BOLD}Invalid username and/or color!{RESET + Colors.RESET}")
         
     else:
-        send(f"{RED + Colors.BOLD}Invalid command usage.{RESET + Colors.RESET}")
+        sender.send(f"{RED + Colors.BOLD}Invalid command usage.{RESET + Colors.RESET}")
