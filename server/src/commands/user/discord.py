7c0a812c9a536b6c06300b32a8803b9274275698
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-09 13:52:27 +0100

    Fixed discord command (user & sender property)

--- server/src/commands/user/discord.py
+++ server/src/commands/user/discord.py
@@ -5,22 +5,22 @@ import socket
 from src.colors import *
 from src.db import Database
 
-from init import server_dir
+from init import User, ClientSender, server_dir
 
 @register_command("discord", arg_count=1)
-def discord_command(socket: socket.socket, username: str, args: list, send):
+def discord_command(socket: socket.socket, user: User, args: list, sender: ClientSender):
     cmd_db = Database(server_dir + "/users.db", check_same_thread=False)
     
     discord_name = args[0]
 
     if discord_name.lower() == "remove":
-        cmd_db.execute("UPDATE users SET discord_name = NULL WHERE username = ?", (username,))
+        cmd_db.execute("UPDATE users SET discord_name = NULL WHERE username = ?", (user.username,))
         cmd_db.commit()
         
-        send(f"{LIGHTGREEN_EX + Colors.BOLD}Removed Discord Link{RESET + Colors.RESET}")
+        sender.send(f"{LIGHTGREEN_EX + Colors.BOLD}Removed Discord Link{RESET + Colors.RESET}")
 
     else:
-        cmd_db.execute("UPDATE users SET discord_name = ? WHERE username = ?", (discord_name, username))
+        cmd_db.execute("UPDATE users SET discord_name = ? WHERE username = ?", (discord_name, user.username))
         cmd_db.commit()
         
-        send(f"{LIGHTGREEN_EX + Colors.BOLD}Changed Discord Link to {MAGENTA}{discord_name}{RESET + Colors.RESET}")   
+        sender.send(f"{LIGHTGREEN_EX + Colors.BOLD}Changed Discord Link to {MAGENTA}{discord_name}{RESET + Colors.RESET}")   
