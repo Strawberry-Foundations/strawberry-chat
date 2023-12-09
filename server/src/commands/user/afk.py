6ed3baf9606b264ab99f2ff6cc07133b7e0370fb
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-09 13:47:45 +0100

    Fixed afk command (user & sender property)

--- server/src/commands/user/afk.py
+++ server/src/commands/user/afk.py
@@ -5,13 +5,13 @@ import socket
 from src.colors import *
 from src.functions import broadcast_all
 
-from init import afks
+from init import User, ClientSender, afks
 
 @register_command("afk")
-def afk_command(socket: socket.socket, username: str, args: list, send):
-    if username in afks:
-        send(f"{YELLOW + Colors.BOLD}You are already AFK!{RESET + Colors.RESET}")
+def afk_command(socket: socket.socket, user: User, args: list, sender: ClientSender):
+    if user.username in afks:
+        sender.send(f"{YELLOW + Colors.BOLD}You are already AFK!{RESET + Colors.RESET}")
         
     else:
-        broadcast_all(f"{username} is now AFK 🌙..")
-        afks.append(username)
+        broadcast_all(f"{user.username} is now AFK 🌙..")
+        afks.append(user.username)
