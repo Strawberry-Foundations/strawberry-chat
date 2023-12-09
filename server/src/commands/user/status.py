0157d98adfb7031d4717ca0dcb223b7ff8ac2f51
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-09 20:16:01 +0100

    Renamed User.status() to User.get_status() & renamed __init__().user_status to status

--- server/src/commands/user/status.py
+++ server/src/commands/user/status.py
@@ -36,7 +36,32 @@ def status_command(socket: socket.socket, user: User, args: list, sender: Client
                     return
                 
         case "get":
-            sender.send(f"{BGREEN}Your current status: {user.user_status}{CRESET}")
+            sender.send(f"{BGREEN}Your current status: {user.status}{CRESET}")
             
         case "system_register":
             sender.send(user.get_system_register())
+            
+@register_command("afk")
+def afk_command(socket: socket.socket, user: User, args: list, sender: ClientSender):
+    if user.status == User.Status.afk:
+        sender.send(f"{YELLOW + Colors.BOLD}You are already AFK!{RESET + Colors.RESET}")
+    else:
+        sender.send(f"{BGREEN}Status set to {YELLOW}afk{CRESET}")
+        user.set_user_status(User.Status.afk)
+        
+@register_command("unafk")
+def unafk_command(socket: socket.socket, user: User, args: list, sender: ClientSender):
+    if user.username not in afks:
+        sender.send(f"{YELLOW + Colors.BOLD}You are not AFK!{RESET + Colors.RESET}")
+
+    else:
+        broadcast_all(f"{user.username} is no longer AFK 🌻!")
+        afks.remove(user.username)
+        
+@register_command("afks")
+@register_command("afklist")
+def afklist_command(socket: socket.socket, user: User, args: list, sender: ClientSender):
+    afkUsers = ', '.join([afks for afks in sorted(afks)])
+    afkUsersLen = len([afks for afks in sorted(afks)])
+    sender.send(f"""{GREEN +  Colors.UNDERLINE + Colors.BOLD}Users who are currently Afk ({afkUsersLen}){RESET + Colors.RESET}
+        {Colors.BOLD}->{Colors.RESET} {CYAN}{afkUsers}{RESET}""")
