72d280000703452b1911d1782bce5734fda9f986
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-09 13:53:41 +0100

    Fixed msgcount command (user & sender property)

--- server/src/commands/user/msgcount.py
+++ server/src/commands/user/msgcount.py
@@ -5,12 +5,12 @@ import socket
 from src.colors import *
 from src.db import Database
 
-from init import server_dir
+from init import User, ClientSender, server_dir
 
 @register_command("msgcount")
-def msgcount_command(socket: socket.socket, username: str, args: list, send):
+def msgcount_command(socket: socket.socket, user: User, args: list, sender: ClientSender):
     cmd_db = Database(server_dir + "/users.db", check_same_thread=False)
-    cmd_db.execute("SELECT msg_count FROM users WHERE username = ?", (username,))
+    cmd_db.execute("SELECT msg_count FROM users WHERE username = ?", (user.username,))
     msg_count = cmd_db.fetchone()
-    send(f"{GREEN + Colors.BOLD}Your message count:{RESET + Colors.RESET} {msg_count[0]}")
+    sender.send(f"{GREEN + Colors.BOLD}Your message count:{RESET + Colors.RESET} {msg_count[0]}")
     cmd_db.cursor_close()
