0e1fe27df2c7785aa039723427c143f18403d09d
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-09 13:47:09 +0100

    Fixed server_settings command (user & sender property)

--- server/src/commands/admin/server_settings.py
+++ server/src/commands/admin/server_settings.py
@@ -7,22 +7,23 @@ from src.db import Database
 from src.vars import server_settings_help
 from src.functions import hash_password, verify_password, escape_ansi
 
-from init import server_dir, ipaddr, port, \
+from init import User, ClientSender, DatabaseConfig, \
+                 server_dir, ipaddr, port, \
                  enable_messages, enable_queue, \
                  max_message_length, max_users, \
                  debug_mode, online_mode, update_channel, \
                  afks, queue, users, addresses, user_logged_in, blacklist, \
                  admins_wait_queue, bots_wait_queue, max_registered_users, \
-                 special_messages, DatabaseConfig
+                 special_messages
 
 
 @register_command("serversettings", arg_count=1, required_permissions=PermissionLevel.ADMIN)
-def user_settings_command(socket: socket.socket, username: str, args: list, send):
+def user_settings_command(socket: socket.socket, user: User, args: list, sender: ClientSender):
     cmd_db = Database(server_dir + "/users.db", check_same_thread=False)
     try:
         match args[0]:
             case "help":
-                send(f"{server_settings_help}{RESET}")
+                sender.send(f"{server_settings_help}{RESET}")
             
             case "show":
                 _base_data = f"""
@@ -71,11 +72,11 @@ def user_settings_command(socket: socket.socket, username: str, args: list, send
                         
                 except: pass
                 
-                send(message)
+                sender.send(message)
             
             case _:
-                send(f"{Colors.RESET + RED}Invalid subcommand!{RESET + Colors.RESET}")
+                sender.send(f"{Colors.RESET + RED}Invalid subcommand!{RESET + Colors.RESET}")
                     
     except Exception as e: 
-        send(f"{RED}Not enough arguments!{RESET}")
+        sender.send(f"{RED}Not enough arguments!{RESET}")
         print(e)
