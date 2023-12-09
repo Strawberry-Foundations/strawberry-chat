da55c521601ddebdb4a85282a798698932912469
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-09 13:53:18 +0100

    Fixed members/users command (user & sender property)

--- server/src/commands/user/members.py
+++ server/src/commands/user/members.py
@@ -5,16 +5,16 @@ import socket
 from src.colors import *
 from src.db import Database
 
-from init import server_dir
+from init import User, ClientSender, server_dir
 
 @register_command("members")
 @register_command("users")
-def members_command(socket: socket.socket, username: str, args: list, send):
+def members_command(socket: socket.socket, user: User, args: list, sender: ClientSender):
     cmd_db = Database(server_dir + "/users.db", check_same_thread=False)
     cmd_db.execute("SELECT username FROM users")
     raw_members = cmd_db.fetchall()
     membersLen = len([raw_members for raw_members in sorted(raw_members)])
     members = ", ".join([result[0] for result in raw_members])
 
-    send(f"""{CYAN +  Colors.UNDERLINE + Colors.BOLD}Members on this server ({membersLen}){RESET + Colors.RESET}
+    sender.send(f"""{CYAN +  Colors.UNDERLINE + Colors.BOLD}Members on this server ({membersLen}){RESET + Colors.RESET}
         {Colors.BOLD}->{Colors.RESET} {CYAN}{members}{RESET}""")
