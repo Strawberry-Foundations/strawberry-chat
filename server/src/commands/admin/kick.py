1fc25d06bcaa745817cd911f76c725ef75c0d6da
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-09 17:46:46 +0100

    Fixed Kick Command: a bytes-like object is required, not 'str'# (117)

--- server/src/commands/admin/kick.py
+++ server/src/commands/admin/kick.py
@@ -3,10 +3,10 @@ from .. import register_command, PermissionLevel
 import socket
 
 from src.colors import *
-from src.functions import doesUserExist, broadcast_all, userRoleColor
+from src.functions import doesUserExist, broadcast_all, userRoleColor, send_json
 from src.db import Database
 
-from init import User, ClientSender, server_dir, users, addresses, log
+from init import User, ClientSender, StbCom, server_dir, users, addresses, log
 
 @register_command("kick", arg_count=1, required_permissions=PermissionLevel.ADMIN)
 def kick_command(socket: socket.socket, user: User, args: list, sender: ClientSender):
@@ -33,14 +33,17 @@ def kick_command(socket: socket.socket, user: User, args: list, sender: ClientSe
     
     else:
         if found_keys:
-            try:
+            # try:
                 log.info(f"{uname} ({addresses[socket][0]}) has been kicked out of the chat by {user.username} for following reason: {reason}")
                 
                 del addresses[to_kick]
                 del users[to_kick]
                 
-                sender.send(f"{YELLOW + Colors.BOLD}Kicked {uname} for following reason: {reason}{RESET + Colors.RESET}")                
-                to_kick.send(f"{YELLOW + Colors.BOLD}You have been kicked out of the chat for the following reason: {reason}{RESET + Colors.RESET}")
+                sender.send(f"{YELLOW + Colors.BOLD}Kicked {uname} for following reason: {reason}{RESET + Colors.RESET}")
+                
+                
+                _to_kick = ClientSender(to_kick)
+                _to_kick.send(f"{YELLOW + Colors.BOLD}You have been kicked out of the chat for the following reason: {reason}{RESET + Colors.RESET}")
                 
                 
                 broadcast_all(f"{Colors.GRAY + Colors.BOLD}<--{Colors.RESET} {userRoleColor(uname)}{uname}{YELLOW + Colors.BOLD} has left the chat room!{RESET + Colors.RESET}")
@@ -51,9 +54,9 @@ def kick_command(socket: socket.socket, user: User, args: list, sender: ClientSe
                     print(f"Could not kick {uname} ({to_kick}) (Closing connection): {e}")
                     pass
                 
-            except Exception as e: 
-                print(f"Could not kick {uname} ({to_kick}): {e}")
-                pass
+            # except Exception as e: 
+            #     print(f"Could not kick {uname} ({to_kick}): {e}")
+            #     pass
             
         else:
             sender.send(f"{RED + Colors.BOLD}User not found or user is offline.{RESET + Colors.RESET}")
