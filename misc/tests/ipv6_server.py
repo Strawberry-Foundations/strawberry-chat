59fac1877f7386cd895e078a4b9b494a3ea498a8
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-10 15:42:09 +0100

    IPv6 Tests

--- misc/tests/ipv6_server.py
+++ misc/tests/ipv6_server.py
@@ -1,9 +1,14 @@
 import socket 
 
+print(socket.getaddrinfo(socket.gethostname(), 9999, socket.AF_INET6))
+
 server = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 server.bind(("2a02:908:e941:3080:b2a4:6e86:6dc4:b59a", 9999))
 
 server.listen()
+
+
+print("Server is running")
 
 while True:
     client, addr = server.accept()
