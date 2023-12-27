ba442c8ff4a1b23fc4c0e6a01d9ae807c32f54fa
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-27 17:05:24 +0100

    updated .gitignore/removed database_test.py

--- server/database_test.py
+++
@@ -1,5 +0,0 @@
-from init import *
-
-db = Database(driver="mysql", database_path="./users.db")
-print(db.fetch_all("SELECT password FROM users WHERE username = ?", ("Julian",)))
-db._execute_query('INSERT INTO users (username, password, role, role_color, enable_blacklisted_words, account_enabled, muted, user_id, msg_count, enable_dms, creation_date) VALUES ("Julian", "test", "member", "red", "true", "true", "false", 0, 0, true, 1);')
