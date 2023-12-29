35e757ed2b1a5922b4d9c93a7213a4a6a3c9343b
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-29 21:04:41 +0100

    init

--- misc/network/ip_address.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def get_global_ip():
-    response = requests.get('https://api.ipify.org?format=json')
-    data = response.json()
-    return data['ip']
-
-print(get_global_ip())
