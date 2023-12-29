33b4a3392de18cd99bc82283d7a636ecd8c8795b
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-29 21:39:42 +0100

    Added open_config()

--- src/utilities.rs
+++ src/utilities.rs
@@ -1,3 +1,8 @@
 use std::fs;
 
-pub fn open_config() {}
+pub fn open_config(config_path: &str) -> String {
+    fs::read_to_string(config_path).unwrap_or_else(|err| {
+        todo!(); // stblib logging
+        std::process::exit(1);
+    })
+}
