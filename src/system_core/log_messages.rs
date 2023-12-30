00a76c088e28bbb8a26713e651c3a7e34d4fed05
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-30 18:06:04 +0100

    added log messages constants

--- src/system_core/log_messages.rs
+++ src/system_core/log_messages.rs
@@ -1,27 +1,23 @@
-pub struct SystemRegister {
-
-}
-
-address_left        = "%s has left"
-user_left           = "%s (%s) has left"
-queue_kick          = "%s got kicked out of the queue"
-queue_left          = "%s (%s) left the queue"
-queue_join          = "%s (%s) is now in the queue"
-connected           = "%s has connected"
-connected_rlm       = "%s (ratelimited) has connected"
-login               = "%s (%s) logged in"
-login_error         = "A login error with %s occured!"
-communication_error = "A communication error with %s (%s) occured!"
-connection_error    = "A connection error occured!"
-transmission_error  = "A message transmission error occurred."
-sql_error           = "An SQL error occured!"
-client_side_error   = "A client-side error occurred."
-stc_error           = "A socket-to-client exception occured"
-registration_error  = "A registration exception occured"
-invalid_sessions_w  = "You should kick some invalid sessions."
-rem_invalid_ses     = "Removed some invalid sessions."
-broadcast_error     = "A broadcasting error occurred."
-runtime_stop        = "Runtime has stopped."
-server_stop         = "Server stopped"
-badge_error         = "Something went wrong while... doing something with the badges?: "
-ratelimit_remoed    = "Ratelimit timeout for %s removed"
+pub const ADDRESS_LEFT: &str        = "%s has left";
+pub const USER_LEFT: &str           = "%s (%s) has left";
+pub const QUEUE_KICK: &str          = "%s got kicked out of the queue";
+pub const QUEUE_LEFT: &str          = "%s (%s) left the queue";
+pub const QUEUE_JOIN: &str          = "%s (%s) is now in the queue";
+pub const CONNECTED: &str           = "%s has connected";
+pub const CONNECTED_RLM: &str       = "%s (ratelimited) has connected";
+pub const LOGIN: &str               = "%s (%s) logged in";
+pub const LOGIN_ERROR: &str         = "A login error with %s occurred!";
+pub const COMMUNICATION_ERROR: &str = "A communication error with %s (%s) occurred!";
+pub const CONNECTION_ERROR: &str    = "A connection error occurred!";
+pub const TRANSMISSION_ERROR: &str  = "A message transmission error occurred.";
+pub const SQL_ERROR: &str           = "An SQL error occurred!";
+pub const CLIENT_SIDE_ERROR: &str   = "A client-side error occurred.";
+pub const STC_ERROR: &str           = "A socket-to-client exception occurred";
+pub const REGISTRATION_ERROR: &str  = "A registration exception occurred";
+pub const INVALID_SESSIONS_W: &str  = "You should kick some invalid sessions.";
+pub const REM_INVALID_SESSION: &str = "Removed some invalid sessions.";
+pub const BROADCAST_ERROR: &str     = "A broadcasting error occurred.";
+pub const RUNTIME_STOP: &str        = "Runtime has stopped.";
+pub const SERVER_STOP: &str         = "Server stopped";
+pub const BADGE_ERROR: &str         = "Something went wrong while... doing something with the badges?: ";
+pub const RATELIMIT_REMOVED: &str   = "Ratelimit timeout for %s removed";
