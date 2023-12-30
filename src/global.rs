a9d89b9bd0e6cc43ae575b7afe1bc01224d014e6
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-30 13:37:10 +0100

    Added stblib::logging

--- src/global.rs
+++ src/global.rs
@@ -2,18 +2,24 @@ use std::env;
 use std::path::{Path, PathBuf};
 use lazy_static::lazy_static;
 
-use crate::config::Config;
+use crate::config::GlobalConfig;
+use stblib::logging::Logger;
 
 lazy_static! {
-    pub static ref CONFIG: Config = {
-        let exe_path = env::current_exe().unwrap_or_else(|err| {
-            todo!(); // stblib logging
-            std::process::exit(1);
+    pub static ref LOGGER: Logger = Logger::new(
+        stblib::logging::featureset::FeatureSet::new(),
+        stblib::logging::formats::strawberry_chat_fmt()
+    );
+
+    pub static ref CONFIG: GlobalConfig = {
+        let exe_path = env::current_exe().unwrap_or_else(|_| {
+            LOGGER.panic("Could not get your Strawberry Chat Runtime Executable");
+            unreachable!()
         });
 
-        let exe_dir = exe_path.parent().unwrap_or_else(|err| {
-            todo!(); // stblib logging
-            std::process::exit(1);
+        let exe_dir = exe_path.parent().unwrap_or_else(|| {
+            LOGGER.panic("Could not get directory of your Strawberry Chat Runtime Executable");
+            unreachable!()
         });
 
         let exe_dir_str = PathBuf::from(exe_dir).display().to_string();
@@ -24,6 +30,6 @@ lazy_static! {
             config_path = String::from("./config.yml")
         }
 
-        Config::new(config_path)
+        GlobalConfig::new(config_path)
     };
 }
