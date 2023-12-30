93591da2347ac057faac9d6387dd20ff7cbe263d
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-30 18:58:03 +0100

    Improvements and fixes

--- src/communication/connection_handler.rs
+++ src/communication/connection_handler.rs
@@ -8,6 +8,8 @@ use tokio::io::AsyncWriteExt;
 use tokio::net::TcpListener;
 
 use crate::global::{CONFIG, LOGGER};
+use crate::system_core::log_parser::log_parser;
+use crate::system_core::log_messages::{CONNECTED, CONNECTED_RLM};
 
 pub async fn connection_handler(socket: TcpListener) {
     let ignore_list: HashMap<IpAddr, u64> = HashMap::new();
@@ -22,7 +24,8 @@ pub async fn connection_handler(socket: TcpListener) {
 
         if CONFIG.networking.ratelimit && ignore_list.contains_key(&client_addr) {
             if !(unix_time() - ignore_list.get(&client_addr).unwrap()) > u64::from(CONFIG.networking.ratelimit_timeout) {
-                LOGGER.info("%s (ratelimited) has connected");
+                LOGGER.info(log_parser(CONNECTED_RLM, &[&client_addr.to_string(),] ));
+
                 client.write_all(
                     format!("{RED}{BOLD}You have been ratelimited due to spam activity. Please try again later{C_RESET}").as_bytes()
                 ).await.unwrap_or_else(|_| {
@@ -33,10 +36,11 @@ pub async fn connection_handler(socket: TcpListener) {
                 LOGGER.info("rlm removed");
             }
         }
+        else {
+            LOGGER.info(log_parser(CONNECTED, &[&client_addr.to_string()]));
+        }
 
-        let peer = client.peer_addr().unwrap().to_string();
-
-        client.write_all(peer.as_bytes()).await.unwrap_or_else(|_| {
+        client.write_all(client.peer_addr().unwrap().to_string().as_bytes()).await.unwrap_or_else(|_| {
            LOGGER.error("");
         });
     }
