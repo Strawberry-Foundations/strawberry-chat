1ec6672b32a37bc78c3ab9cda65f13c241ab2d47
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-30 20:59:14 +0100

    added ClientSender::send

--- src/system_core/user.rs
+++ src/system_core/user.rs
@@ -1,5 +1,9 @@
 use std::net::IpAddr;
+use tokio::io::AsyncWriteExt;
 use tokio::net::TcpStream;
+use crate::constants::log_messages::COMMUNICATION_ERROR;
+use crate::global::LOGGER;
+use crate::system_core::log::log_parser;
 
 pub struct ClientSender {
     pub socket: TcpStream,
@@ -14,5 +18,15 @@ impl ClientSender {
             socket: client,
             address: *addr
         }
+    }
+
+    pub async fn send(&mut self, message: impl ToString) {
+        self.socket.write_all(
+            message.to_string().as_bytes()).await.unwrap_or_else(
+            |_| LOGGER.error(
+                log_parser(COMMUNICATION_ERROR, &[&self.address, &"placeholder"]
+                )
+            )
+        );
     }
 }
