6227b8c439479d076ad660876eec45a89e8545a3
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-30 16:15:16 +0100

    Moved runtime_logger to globalrs

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,14 @@
 
 use std::error::Error;
 
-use stblib::colors::{BOLD, C_RESET, CYAN, RED, RESET};
-use stblib::logging::formats::{LogFormat, LogFormatExt};
-use stblib::logging::Logger;
+use stblib::colors::{BOLD, C_RESET, CYAN, RESET};
 
 use tokio::net::TcpListener;
 
-use crate::global::{CHAT_NAME, CODENAME, CONFIG, DEFAULT_VERSION, SERVER_EDITION};
+use crate::global::{
+    CHAT_NAME, CODENAME, CONFIG, DEFAULT_VERSION,
+    RUNTIME_LOGGER, SERVER_EDITION
+};
 
 mod config;
 mod utilities;
@@ -19,34 +20,20 @@ mod cli_wins;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let runtime_logger = Logger::new(
-        stblib::logging::featureset::FeatureSet::new(),
-        LogFormat {
-            info: String::new(),
-            error: format!("{C_RESET}{BOLD}{RED}RUNTIME_ERROR{C_RESET} [%<message>%]"),
-            default: String::new(),
-            warning: String::new(),
-            critical: String::new(),
-            extensions: LogFormatExt {
-                time_fmt: "%Y-%m-%d %H:%M".to_string(),
-                levelname_lowercase: false
-            },
-        }
-    );
-
     println!("{CYAN}{BOLD}* -- {CHAT_NAME} {} {CODENAME} ({SERVER_EDITION}) -- *{RESET}{C_RESET}", DEFAULT_VERSION.clone());
 
     let _socket = TcpListener::bind((CONFIG.server.address.clone(), CONFIG.server.port)).await.unwrap_or_else(|err| {
-        runtime_logger.panic(format!("{}", err));
+        RUNTIME_LOGGER.panic(format!("{}", err));
         unreachable!()
     });
-
 
     cli_wins::feature::display();
 
     if !CONFIG.flags.online_mode {
         cli_wins::online_mode::display();
     }
+
+
 
 
 
