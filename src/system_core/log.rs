9399e6dd177bb0702976a50636527b6a4969c003
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-30 20:04:39 +0100

    Improved code

--- src/system_core/log.rs
+++ src/system_core/log.rs
@@ -5,10 +5,10 @@ pub fn log_parser(log_message: &str, params: &[&dyn std::fmt::Display]) -> Strin
 
         for param in params {
             if let Some(index) = formatted_message.find("%s") {
-                formatted_message.replace_range(index..(index + 2), &format!("{param}"));
+                formatted_message.replace_range(index..(index + 2), &param.to_string());
             }
             else if let Some(index) = formatted_message.find("%d") {
-                if let Ok(value) = format!("{param}").parse::<i64>() {
+                if let Ok(value) = param.to_string().parse::<i64>() {
                     formatted_message.replace_range(index..(index + 2), &value.to_string());
                 }
             }
