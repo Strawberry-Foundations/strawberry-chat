0fa945393f61a15eacf5a84540b2b20d57425ea9
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-30 15:04:52 +0100

    some tests

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,19 @@
 #![allow(clippy::module_name_repetitions, clippy::should_implement_trait, clippy::struct_excessive_bools)]
 #![allow(dead_code)]
 
-use crate::global::CONFIG;
+use std::error::Error;
+use stblib::colors::{BOLD, C_RESET, CYAN, RESET};
+use tokio::net::TcpListener;
+use crate::global::{CHAT_NAME, CODENAME, CONFIG, DEFAULT_VERSION, EXT_VERSION, SERVER_EDITION};
 
 mod config;
 mod utilities;
 mod global;
 
-fn main() {
-    println!("{}:{}", CONFIG.server.address, CONFIG.server.port);
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    println!("{CYAN}{BOLD}* -- {CHAT_NAME} {} {CODENAME} ({SERVER_EDITION}) -- *{RESET}{C_RESET}", DEFAULT_VERSION.clone());
+    println!("{}", EXT_VERSION.clone());
+    let socket = TcpListener::bind((CONFIG.server.address.clone(), CONFIG.server.port)).await?;
+    Ok(())
 }
