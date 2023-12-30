a349a77993286402adf4a5ee71f98ff9437c7f75
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-30 21:37:48 +0100

    Implementationf or SysteMessage

--- src/system_core/packages.rs
+++ src/system_core/packages.rs
@@ -1,5 +1,25 @@
 use crate::system_core::types::PackageTypes;
 
+pub struct MessageStruct {
+    content: String
+}
+
 pub struct SystemMessage {
-    message_type: PackageTypes
+    message_type: PackageTypes,
+    message: MessageStruct,
+}
+
+impl SystemMessage {
+    pub const fn new() -> Self {
+        Self {
+            message_type: PackageTypes::SystemMessage,
+            message: MessageStruct {
+                content: String::new()
+            }
+        }
+    }
+
+    pub fn write(&self, message: impl ToString) -> String {
+        message.to_string()
+    }
 }
