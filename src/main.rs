53d3104f8a99db50585ff0d6ebfa1c853634c3c1
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-30 16:12:07 +0100

    Fixed imports + moved "windows" to separate files

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,19 @@
 #![allow(dead_code)]
 
 use std::error::Error;
-use stblib::colors::{BOLD, C_RESET, CYAN, RED, RESET, WHITE, YELLOW};
+
+use stblib::colors::{BOLD, C_RESET, CYAN, RED, RESET};
 use stblib::logging::formats::{LogFormat, LogFormatExt};
 use stblib::logging::Logger;
+
 use tokio::net::TcpListener;
+
 use crate::global::{CHAT_NAME, CODENAME, CONFIG, DEFAULT_VERSION, SERVER_EDITION};
-use crate::utilities::{get_ratelimit_timeout, is_feature_enabled};
 
 mod config;
 mod utilities;
 mod global;
+mod cli_wins;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -38,17 +41,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
         unreachable!()
     });
 
-    println!("\n{BOLD}  {CYAN}* -------------- FEATURES -------------- *{RESET}{C_RESET}");
-    println!("{BOLD}  {CYAN}|{WHITE} *{YELLOW} Console Message Logging is {}{CYAN}  |{RESET}{C_RESET}",
-             is_feature_enabled(CONFIG.flags.enable_messages)
-    );
-    println!("{BOLD}  {CYAN}|{WHITE} *{YELLOW} Debug Mode is {}             {CYAN}  |{RESET}{C_RESET}",
-             is_feature_enabled(CONFIG.flags.debug_mode)
-    );
-    println!("{BOLD}  {CYAN}|{WHITE} *{YELLOW} Ratelimit is {}{}        {CYAN}|{RESET}{C_RESET}",
-             is_feature_enabled(CONFIG.networking.ratelimit), get_ratelimit_timeout(CONFIG.networking.ratelimit)
-    );
-    println!("{BOLD}  {CYAN}* -------------------------------------- *{RESET}{C_RESET}\n");
+
+    cli_wins::feature::display();
+
+    if !CONFIG.flags.online_mode {
+        cli_wins::online_mode::display();
+    }
 
 
 
