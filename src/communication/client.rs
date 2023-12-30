85c593611b1a9e2d7b61fb58966781f23c6b59f8
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-30 20:59:27 +0100

    improvemente

--- src/communication/client.rs
+++ src/communication/client.rs
@@ -8,13 +8,13 @@ use crate::global::{CONFIG, LOGGER};
 use crate::system_core::user;
 
 pub async fn client_handler(mut client: TcpStream) {
-    let sender = user::ClientSender::new(client);
-
-    if CONFIG.security.banned_ips.contains(&sender.address.to_string()) {
+    if CONFIG.security.banned_ips.contains(&client.peer_addr().unwrap().ip().clone().to_string()) {
         client.write_all(format!("{RED}{BOLD}Sorry, you're not allowed to connect to this server.{C_RESET}").as_bytes()).await.expect("");
         client.shutdown().await.unwrap_or_else(|_| LOGGER.error(STC_ERROR));
         return
     }
 
-    client.write_all(b"hehehe").await.expect("");
+    let mut sender = user::ClientSender::new(client);
+
+    sender.send("Hello").await;
 }
