7ff59f6072843c3d81649f4e46ef4917596bbcf4
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-30 21:26:28 +0100

    starting login proces

---
+++ src/system_core/login.rs
@@ -0,0 +1,9 @@
+use stblib::colors::{BOLD, C_RESET, MAGENTA, RESET};
+use stblib::utilities::{ms_sleep};
+use crate::system_core::user::ClientSender;
+
+pub async fn client_login(sender: &mut ClientSender) {
+    sender.send(format!("{C_RESET}{BOLD}Welcome to Strawberry Chat!{C_RESET}")).await;
+    ms_sleep(80);
+    sender.send(format!("{C_RESET}{BOLD}New here? Type '{MAGENTA}Register{RESET}' to register! You want to leave? Type '{MAGENTA}Exit{RESET}' {C_RESET}")).await;
+}
