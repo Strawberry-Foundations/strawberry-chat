79ad8532bd878b62f851ed1cf355131ce501d679
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-30 20:32:27 +0100

    added sender var

--- src/communication/client.rs
+++ src/communication/client.rs
@@ -5,12 +5,12 @@ use stblib::colors::{BOLD, C_RESET, RED};
 use crate::constants::log_messages::STC_ERROR;
 
 use crate::global::{CONFIG, LOGGER};
+use crate::system_core::user;
 
 pub async fn client_handler(mut client: TcpStream) {
-    let sender =
-    let client_addr = client.peer_addr().unwrap().ip();
+    let sender = user::ClientSender::new(client);
 
-    if CONFIG.security.banned_ips.contains(&client_addr.to_string()) {
+    if CONFIG.security.banned_ips.contains(&sender.address.to_string()) {
         client.write_all(format!("{RED}{BOLD}Sorry, you're not allowed to connect to this server.{C_RESET}").as_bytes()).await.expect("");
         client.shutdown().await.unwrap_or_else(|_| LOGGER.error(STC_ERROR));
         return
