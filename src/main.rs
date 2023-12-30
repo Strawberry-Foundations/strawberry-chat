a4e14d1e8fd9fe8bd4951448562c329aef8f2e49
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-30 13:50:19 +0100

    Fixed some clippy warnings

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+#![warn(clippy::all, clippy::nursery, clippy::pedantic)]
+#![allow(clippy::module_name_repetitions, clippy::should_implement_trait, clippy::struct_excessive_bools)]
+#![allow(dead_code)]
+
+use crate::global::CONFIG;
+
 mod config;
 mod utilities;
 mod global;
 
 fn main() {
-    println!("Hello, world!");
+    println!("{}:{}", CONFIG.server.address, CONFIG.server.port);
 }
