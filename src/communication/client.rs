773123769e411db7a440e1241f8ff7ae4c98afa2
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-30 19:45:15 +0100

    starting client handling

--- src/communication/client.rs
+++ src/communication/client.rs
@@ -1,3 +1,6 @@
-pub async fn client_handler() {
-    todo!()
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpStream;
+
+pub async fn client_handler(mut client: TcpStream) {
+    client.write_all(b"hehehe").await.expect("");
 }
