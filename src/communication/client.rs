d4fa4ef01084fde6784d507536600c7e1c556047
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-30 20:30:40 +0100

    added ip blocking system

--- src/communication/client.rs
+++ src/communication/client.rs
@@ -1,6 +1,20 @@
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpStream;
 
+use stblib::colors::{BOLD, C_RESET, RED};
+use crate::constants::log_messages::STC_ERROR;
+
+use crate::global::{CONFIG, LOGGER};
+
 pub async fn client_handler(mut client: TcpStream) {
+    let sender =
+    let client_addr = client.peer_addr().unwrap().ip();
+
+    if CONFIG.security.banned_ips.contains(&client_addr.to_string()) {
+        client.write_all(format!("{RED}{BOLD}Sorry, you're not allowed to connect to this server.{C_RESET}").as_bytes()).await.expect("");
+        client.shutdown().await.unwrap_or_else(|_| LOGGER.error(STC_ERROR));
+        return
+    }
+
     client.write_all(b"hehehe").await.expect("");
 }
