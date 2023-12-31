8e98f42c9cf303dc61faf55beadc7f49d23964b8
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-31 19:19:38 +0100

    renamed package* to packet* to make someone... happy

--- src/system_core/login.rs
+++ src/system_core/login.rs
@@ -1,16 +1,16 @@
 use stblib::colors::{BOLD, C_RESET, MAGENTA, RED, RESET};
 
-use crate::system_core::packages::{EventBackend, Package};
+use crate::system_core::packet::{EventBackend, Packet};
 use crate::system_core::types::LOGIN_EVENT;
 use crate::system_core::user::{ClientSender, UserObject};
 
 pub async fn client_login(sender: &mut ClientSender) {
-    let mut package = Package::new();
-    let mut login_package = EventBackend::new_predefined(&LOGIN_EVENT);
+    let mut packet = Packet::new();
+    let mut login_packet = EventBackend::new_predefined(&LOGIN_EVENT);
 
-    sender.send(package.system.write(&format!("{C_RESET}{BOLD}Welcome to Strawberry Chat!{C_RESET}"))).await;
-    sender.send(package.system.write(&format!("{C_RESET}{BOLD}New here? Type '{MAGENTA}Register{RESET}' to register! You want to leave? Type '{MAGENTA}Exit{RESET}' {C_RESET}"))).await;
-    sender.send(login_package.push()).await;
+    sender.send(packet.system.write(&format!("{C_RESET}{BOLD}Welcome to Strawberry Chat!{C_RESET}"))).await;
+    sender.send(packet.system.write(&format!("{C_RESET}{BOLD}New here? Type '{MAGENTA}Register{RESET}' to register! You want to leave? Type '{MAGENTA}Exit{RESET}' {C_RESET}"))).await;
+    sender.send(login_packet.push()).await;
 
     let user_object = UserObject {
         username: "julian".to_string(),
@@ -20,5 +20,5 @@ pub async fn client_login(sender: &mut ClientSender) {
         avatar_url: "https://media.discordapp.net/attachments/874284875618844766/1175912845641265242/WhatsApp_Bild_2023-08-18_um_19.55.41_1.jpg".to_string(),
     };
 
-    sender.send(package.user.write(user_object, &"Hi")).await;
+    sender.send(packet.user.write(user_object, &"Hi")).await;
 }
