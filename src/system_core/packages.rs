f57660302af2360920cade0ef03f080f3f4e3689
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-31 13:50:44 +0100

    added UserObject struct

--- src/system_core/packages.rs
+++ src/system_core/packages.rs
@@ -15,12 +15,25 @@ pub struct SystemMessage {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct UserMessage {
     pub message_type: String,
+    pub username: String,
+    pub nickname: String,
+    pub badge: String,
+    pub role_color: String,
+    pub avatar_url: String,
     pub message: MessageStruct,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct MessageStruct {
     pub content: String
+}
+
+pub struct UserObject {
+    pub username: String,
+    pub nickname: String,
+    pub badge: String,
+    pub role_color: String,
+    pub avatar_url: String,
 }
 
 
@@ -35,6 +48,11 @@ impl Package {
             },
             user: UserMessage {
                 message_type: USER_MESSAGE.to_string(),
+                username: String::new(),
+                nickname: String::new(),
+                badge: String::new(),
+                role_color: String::new(),
+                avatar_url: String::new(),
                 message: MessageStruct {
                     content: String::new()
                 },
@@ -53,9 +71,36 @@ impl SystemMessage {
         }
     }
 
-    pub fn write(&mut self, message: impl ToString) -> String {
+    pub fn write(&mut self, message: &impl ToString) -> String {
         self.message.content = message.to_string();
+        serde_json::to_string(self).unwrap()
+    }
+}
+
+impl UserMessage {
+    pub fn new() -> Self {
+        Self {
+            message_type: USER_MESSAGE.to_string(),
+            username: String::new(),
+            nickname: String::new(),
+            badge: String::new(),
+            role_color: String::new(),
+            avatar_url:  String::new(),
+            message: MessageStruct {
+                content: String::new()
+            },
+        }
+    }
+
+    pub fn write(&mut self, user_object: UserObject, message: &impl ToString) -> String {
+        self.username   = user_object.username;
+        self.nickname   = user_object.nickname;
+        self.badge      = user_object.badge;
+        self.role_color = user_object.role_color;
+        self.avatar_url = user_object.avatar_url;
+
         self.message.content = message.to_string();
+
 
         serde_json::to_string(self).unwrap()
     }
