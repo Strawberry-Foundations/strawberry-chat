a7c3605cfd6cf0551d7e51130edff38d7fb975fe
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-31 17:41:31 +0100

    added more stbchat datatypes

--- src/system_core/types.rs
+++ src/system_core/types.rs
@@ -1,2 +1,7 @@
 pub const SYSTEM_MESSAGE: &str = "system_message";
 pub const USER_MESSAGE: &str = "user_message";
+
+pub const STBCHAT_NOTIFICATION: &str = "stbchat_notification";
+pub const STBCHAT_BACKEND: &str = "stbchat_backend";
+pub const STBCHAT_LOGIN: &str = "stbchat_login";
+pub const STBCHAT_REGISTER: &str = "stbchat_register";
