fe4a6e99823140256895f784323880e422ea441a
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-31 13:36:48 +0100

    added more packages

--- src/system_core/packages.rs
+++ src/system_core/packages.rs
@@ -1,25 +1,62 @@
-use crate::system_core::types::PackageTypes;
+use serde::{Deserialize, Serialize};
+use crate::system_core::types::{SYSTEM_MESSAGE, USER_MESSAGE};
 
-pub struct MessageStruct {
-    content: String
+pub struct Package {
+    pub system: SystemMessage,
+    pub user: UserMessage,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct SystemMessage {
-    message_type: PackageTypes,
-    message: MessageStruct,
+    pub message_type: String,
+    pub message: MessageStruct,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserMessage {
+    pub message_type: String,
+    pub message: MessageStruct,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MessageStruct {
+    pub content: String
+}
+
+
+impl Package {
+    pub fn new() -> Self {
+        Self {
+            system: SystemMessage {
+                message_type: SYSTEM_MESSAGE.to_string(),
+                message: MessageStruct {
+                    content: String::new()
+                }
+            },
+            user: UserMessage {
+                message_type: USER_MESSAGE.to_string(),
+                message: MessageStruct {
+                    content: String::new()
+                },
+            },
+        }
+    }
 }
 
 impl SystemMessage {
-    pub const fn new() -> Self {
+    pub fn new() -> Self {
         Self {
-            message_type: PackageTypes::SystemMessage,
+            message_type: SYSTEM_MESSAGE.to_string(),
             message: MessageStruct {
                 content: String::new()
             }
         }
     }
 
-    pub fn write(&self, message: impl ToString) -> String {
-        message.to_string()
+    pub fn write(&mut self, message: impl ToString) -> String {
+        self.message.content = message.to_string();
+        self.message.content = message.to_string();
+
+        serde_json::to_string(self).unwrap()
     }
 }
