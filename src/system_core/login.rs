d3a89fffadb87906a1fd8a88e5b8a18fc897a2eb
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-31 13:35:50 +0100

    Some updates

--- src/system_core/login.rs
+++ src/system_core/login.rs
@@ -1,8 +1,13 @@
 use stblib::colors::{BOLD, C_RESET, MAGENTA, RESET};
 use stblib::utilities::{ms_sleep};
+use crate::system_core::packages::Package;
 use crate::system_core::user::ClientSender;
 
 pub async fn client_login(sender: &mut ClientSender) {
+    let mut package = Package::new();
+
+    sender.send(package.system.write("s")).await;
+
     sender.send(format!("{C_RESET}{BOLD}Welcome to Strawberry Chat!{C_RESET}")).await;
     ms_sleep(80);
     sender.send(format!("{C_RESET}{BOLD}New here? Type '{MAGENTA}Register{RESET}' to register! You want to leave? Type '{MAGENTA}Exit{RESET}' {C_RESET}")).await;
