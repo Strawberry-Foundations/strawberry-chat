935e690fb9e482e5192df5ebb696e307e6e89d14
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-10 22:50:28 +0000

    Added example command

---
+++ src/commands/etc/test_command.rs
@@ -0,0 +1,18 @@
+use crate::system_core::commands;
+
+pub fn hello_command() -> commands::Command {
+    fn logic(ctx: &commands::Context) -> Result<String, String> {
+        Ok({
+            format!("Username: {}", ctx.executor.username);
+            format!("Args: {:?}", ctx.args)
+        })
+    }
+
+    commands::Command {
+        name: "test".to_string(),
+        description: "Example command".to_string(),
+        handler: |ctx| Box::pin(async move {
+            logic(&ctx)
+        }),
+    }
+}
