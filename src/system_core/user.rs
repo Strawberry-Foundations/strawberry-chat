335a469c774bdd119127c91d3c8b286fe7bd64c0
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-10 23:13:37 +0000

    Moved system_core::user::UserObject to system_core::objects::UserObject

--- src/system_core/user.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Clone, Default, Debug)]
-pub struct UserObject {
-    pub username: String,
-    pub nickname: String,
-    pub badge: char,
-    pub role_color: String,
-    pub avatar_url: String,
-}
