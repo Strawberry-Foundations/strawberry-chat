f9fbbfc4e3ddf1e513b5c1f3366e5d48529ab187
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-11 19:31:19 +0000

    some rewrites to command system

--- src/commands/etc/test_command.rs
+++ src/commands/etc/test_command.rs
@@ -2,9 +2,9 @@ use crate::system_core::commands;
 // use crate::system_core::message::MessageToClient;
 
 pub fn example_command() -> commands::Command {
-    fn logic(ctx: &commands::Context) -> Result<String, String> {
+    fn logic(ctx: &commands::Context) -> Result<Option<String>, String> {
         // ctx.tx_channel.send(MessageToClient::SystemMessage { content: "Hello".into() }).unwrap();
-        Ok(format!("\n  Username: {}\n  Args: {:?}", ctx.executor.username, ctx.args))
+        Ok(Some(format!("\n  Username: {}\n  Args: {:?}", ctx.executor.username, ctx.args)))
     }
 
     commands::Command {
