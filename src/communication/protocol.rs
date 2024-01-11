b46e2b9707da4a4de258099f7a26735ffde5f450
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-11 19:02:50 +0000

    Moved JsonStreamDeserializer to communication/protocol

--- src/communication/protocol.rs
+++ src/communication/protocol.rs
@@ -1,3 +1,6 @@
+use serde::de::DeserializeOwned;
+use tokio::io::{AsyncRead, AsyncReadExt};
+
 pub struct BlockedWords<'a> {
     pub blacklisted_words: Vec<&'a str>,
     pub content_block: Vec<&'a str>,
@@ -7,6 +10,11 @@ pub struct MessageVerification {
     pub blocked_words: BlockedWords<'static>,
 }
 
+pub struct JsonStreamDeserializer<R: AsyncRead + Unpin> {
+    reader: R
+}
+
+
 impl MessageVerification {
     pub fn new() -> Self {
         Self {
@@ -15,5 +23,54 @@ impl MessageVerification {
                 content_block: vec!["[#<keepalive.event.sent>]"],
             }
         }
+    }
+}
+
+impl<R: AsyncRead + Unpin + Send> JsonStreamDeserializer<R> {
+    pub const fn from_read(reader: R) -> Self {
+        Self { reader }
+    }
+
+    pub async fn next<T: DeserializeOwned + Send>(&mut self) -> eyre::Result<T> {
+        let mut bytes = vec![];
+
+        let mut wraps = 0u32;
+        let mut in_string = false;
+        let mut escape = false;
+        loop {
+            let byte = self.reader.read_u8().await?;
+            if byte != b'{' && wraps == 0 {
+                continue;
+            }
+            match byte {
+                b'{' => {
+                    if !in_string {
+                        wraps += 1;
+                    }
+                },
+                b'}' => {
+                    wraps -= 1;
+                    if wraps == 0 {
+                        bytes.push(byte);
+                        break;
+                    }
+                },
+                b'\\' => {
+                    escape = true;
+                },
+                b'"' => {
+                    if escape {
+                        escape = false;
+                    } else {
+                        in_string ^= true;
+                    }
+                }
+                _ => {}
+            }
+            bytes.push(byte);
+        }
+        let string = String::from_utf8(bytes)?;
+        // dbg!(&string);
+        Ok(serde_json::from_str(&string)?)
     }
 }
