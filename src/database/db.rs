4bbd7eca6748e5379acc39ce4ab86ae855e68d2f
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-13 15:39:59 +0000

    Rewrite of database system

---
+++ src/database/db.rs
@@ -0,0 +1,19 @@
+use lazy_static::lazy_static;
+use sqlx::{MySql, Pool};
+
+use crate::database::Database;
+use crate::global::CONFIG;
+
+lazy_static!(
+    pub static ref DATABASE: Database = futures::executor::block_on(async {
+        let url = format!(
+            "mysql://{}:{}@{}/{}",
+            CONFIG.database.user, CONFIG.database.password,
+            CONFIG.database.host, CONFIG.database.database_name
+        );
+
+        Database::new(url.as_str()).await
+    });
+
+    pub static ref CONNECTION: Pool<MySql> = DATABASE.connection.clone();
+);
