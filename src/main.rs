b11b2ab449c4f0297fbab4fbf2792b435666a630
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-13 15:48:33 +0000

    added connection testing

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,14 @@
 #![allow(clippy::module_name_repetitions, clippy::should_implement_trait, clippy::struct_excessive_bools, dead_code, unused_doc_comments, clippy::missing_const_for_fn)]
 
 use std::error::Error;
-use sqlx::{MySql, Pool};
 use tokio::net::TcpListener;
 use tokio::spawn;
 
 use stblib::colors::{BOLD, C_RESET, CYAN, ITALIC, MAGENTA, RESET};
 
 use crate::communication::connection_handler::connection_handler;
+use crate::database::db::DATABASE;
 use crate::global::{CHAT_NAME, CODENAME, CONFIG, DEFAULT_VERSION, RUNTIME_LOGGER, SERVER_EDITION};
-use crate::system_core::db::DATABASE;
 use crate::system_core::server_core::core_thread;
 use crate::utilities::runtime_all_addresses;
 
@@ -21,15 +20,10 @@ mod communication;
 mod system_core;
 mod constants;
 mod commands;
-
-fn init_db() -> Pool<MySql>{
-    DATABASE.clone()
-}
+mod database;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    init_db();
-
     println!("{CYAN}{BOLD}* -- {CHAT_NAME} {} {CODENAME} ({SERVER_EDITION}) -- *{RESET}{C_RESET}", DEFAULT_VERSION.clone());
 
     let socket = TcpListener::bind((CONFIG.server.address.clone(), CONFIG.server.port)).await.unwrap_or_else(|err| {
@@ -42,6 +36,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
     if !CONFIG.flags.online_mode {
         cli_wins::online_mode::display();
     }
+
+    RUNTIME_LOGGER.default(format!("Connecting to database on address {}", CONFIG.database.host));
+    let _ = DATABASE.connection.clone();
+
+    RUNTIME_LOGGER.default("Connected");
+
 
     RUNTIME_LOGGER.info(
         format!(
