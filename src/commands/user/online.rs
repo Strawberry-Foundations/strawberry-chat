c04b101fdf13fca112f9c7e9b21a8948c3ddee5c
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-20 19:13:44 +0000

    Added Core Struct for important data that needs to be read while runtime

--- src/commands/user/online.rs
+++ src/commands/user/online.rs
@@ -1,21 +1,17 @@
 use stblib::colors::{BOLD, C_RESET, CYAN, GREEN, UNDERLINE};
 
 use crate::global::CONFIG;
-use crate::system_core::commands;
+use crate::system_core::{commands, CORE};
 use crate::system_core::message::MessageToClient;
 use crate::system_core::server_core::{get_online_users, get_users_len};
 
 pub fn online() -> commands::Command {
     async fn logic(ctx: &commands::Context) -> commands::CommandResponse {
-        println!("Test #1");
-
         let online_users = if CONFIG.config.max_users == -1 {
-            format!("{}", "get_users_len().await")
+            format!("{}", CORE.read().await.online_users)
         } else {
-            format!("{}/{}", "get_users_len().await", CONFIG.config.max_users)
+            format!("{}/{}", CORE.read().await.online_users, CONFIG.config.max_users)
         };
-
-        println!("Test #2: {online_users}");
 
         let message = format!("{GREEN}{BOLD}{UNDERLINE}Users who are currently online ({online_users}){C_RESET}
         {BOLD}->{C_RESET} {CYAN}{}{C_RESET}
