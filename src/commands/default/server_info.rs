2261a32a50c717102c7913b58a8da0a1690843c5
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-20 15:49:35 +0000

    Added server_info command

---
+++ src/commands/default/server_info.rs
@@ -0,0 +1,23 @@
+use stblib::colors::{BOLD, C_RESET, WHITE};
+use crate::global::CONFIG;
+use crate::system_core::commands;
+use crate::system_core::message::MessageToClient;
+
+pub fn server_info() -> commands::Command {
+    async fn logic(ctx: &commands::Context) -> Result<Option<String>, String> {
+        ctx.tx_channel.send(MessageToClient::SystemMessage {
+            content: format!("{WHITE}{BOLD}{}{C_RESET}", CONFIG.server.description)
+        }).await.unwrap();
+
+        Ok(None)
+    }
+
+    commands::Command {
+        name: "serverinfo".to_string(),
+        aliases: vec!["server-info", "info"],
+        description: "Show information about this server".to_string(),
+        handler: |ctx| Box::pin(async move {
+            logic(&ctx).await
+        }),
+    }
+}
