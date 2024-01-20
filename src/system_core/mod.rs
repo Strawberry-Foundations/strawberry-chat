670fe5421e721603a7fe9cdbc409e13073930be7
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-20 19:13:45 +0000

    Added Core Struct for important data that needs to be read while runtime

--- src/system_core/mod.rs
+++ src/system_core/mod.rs
@@ -1,3 +1,7 @@
+use tokio::sync::RwLock;
+use lazy_static::lazy_static;
+use crate::system_core::server_core::Core;
+
 pub mod log;
 pub mod config;
 pub mod login;
@@ -7,3 +11,7 @@ pub mod objects;
 pub mod message;
 pub mod server_core;
 pub mod commands;
+
+lazy_static! {
+    pub static ref CORE: RwLock<Core> = RwLock::new(Core::new());
+}
