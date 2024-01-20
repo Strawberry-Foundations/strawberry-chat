66fb8d82da123aca35900e151a567f1e6ecf1c65
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-20 17:40:59 +0000

    Added src/security + moved message verification to src/security/automod

--- src/communication/protocol.rs
+++ src/communication/protocol.rs
@@ -1,49 +1,8 @@
 use serde::de::DeserializeOwned;
 use tokio::io::{AsyncRead, AsyncReadExt};
 
-pub struct BlockedWords<'a> {
-    pub blacklisted_words: Vec<&'a str>,
-    pub content_block: Vec<&'a str>,
-}
-
-pub struct MessageVerificatior {
-    pub blocked_words: BlockedWords<'static>,
-}
-
-#[derive(PartialEq, Eq)]
-pub enum MessageAction {
-    Allow,
-    Hide,
-    Kick
-}
-
-impl MessageVerificatior {
-    pub fn check(&self, content: &impl ToString) -> MessageAction {
-        let content = content.to_string();
-        if self.blocked_words.blacklisted_words.iter().any(|w| content.contains(w)) {
-            return MessageAction::Kick;
-        }
-        if self.blocked_words.content_block.iter().any(|w| content.contains(w)) {
-            return MessageAction::Hide;
-        }
-        MessageAction::Allow
-    }
-}
-
 pub struct JsonStreamDeserializer<R: AsyncRead + Unpin> {
     reader: R
-}
-
-
-impl MessageVerificatior {
-    pub fn new() -> Self {
-        Self {
-            blocked_words: BlockedWords {
-                blacklisted_words: vec!["gullideckel"],
-                content_block: vec!["[#<keepalive.event.sent>]"],
-            }
-        }
-    }
 }
 
 impl<R: AsyncRead + Unpin + Send> JsonStreamDeserializer<R> {
