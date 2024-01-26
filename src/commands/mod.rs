b68870db6edfd52ee13a3fe426fc9973aecb7779
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-26 14:17:29 +0000

    Added command categories

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,14 +1,16 @@
 #![allow(clippy::unnecessary_wraps)]
 
-pub mod etc {
-    pub mod test_command;
-}
-
 pub mod default {
     pub mod server_info;
     pub mod about;
+    pub mod help;
 }
 
 pub mod user {
     pub mod online;
 }
+
+pub mod etc {
+    pub mod test_command;
+}
+
