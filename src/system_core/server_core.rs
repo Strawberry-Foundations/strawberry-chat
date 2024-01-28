a31aca6f8a7acb8df1eca026f0f9ffab70ddf09a
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-28 21:35:11 +0000

    fixed formatting

--- src/system_core/server_core.rs
+++ src/system_core/server_core.rs
@@ -39,14 +39,10 @@ pub async fn get_online_usernames() -> Vec<String> {
     CLIENTS.read().await.iter().filter_map(|c| c.get_user().map(|u| u.username)).collect()
 }
 
-pub async fn register_connection(
-    peer: SocketAddr,
-) -> (
-    Sender<MessageToServer>,
-    Receiver<MessageToClient>,
-) {
+pub async fn register_connection(peer: SocketAddr) -> (Sender<MessageToServer>, Receiver<MessageToClient>, ) {
     let (tc_tx, tc_rx) = channel::<MessageToClient>(CHANNEL_BUFFER);
     let (ts_tx, ts_rx) = channel::<MessageToServer>(CHANNEL_BUFFER);
+
     CLIENTS.write().await.push(
         Connection {
             state: State::Unauthorized,
@@ -55,6 +51,7 @@ pub async fn register_connection(
             peer_addr: peer
         }
     );
+
     (ts_tx, tc_rx)
 }
 
