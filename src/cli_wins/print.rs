e63c36348ff71ecf97d337cd4eed5e8b70f44d97
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-28 23:58:43 +0000

    Added constructor for cli_wins

--- src/cli_wins/print.rs
+++ src/cli_wins/print.rs
@@ -17,24 +17,40 @@
 
 use stblib::colors::*;
 
+/// * --------- *
+/// |    Test    |
+///
+
 pub fn print_window(txt: &str) {
     let margin = if txt.len() % 2 == 0 { "  " } else { " " };
 
-    print!(" *");
-    for txt in 0..(txt.len() + 5) {
+    println!("\
+--- CLIWins 0.1.0 Debug Information ---
+Text Length: {}
+Margin Length: {}
+    ", txt.len(), margin.len()
+    );
+
+    print!(" * ");
+    for txt in 0..(txt.len()) {
         print!("-");
     }
-    print!("*");
+    print!(" *");
+
+
     println!();
-    print!(" |");
-    for txt in 0..(txt.len() / 2) {
+    print!(" | ");
+
+    /* for txt in 0..(txt.len() / 2) {
         print!("{margin}");
-    }
+    } */
+
     print!("{txt}");
+
     for txt in 0..(txt.len() / 2) {
         print!("{margin}");
     }
-    print!("|");
+    print!(" |");
     println!();
     print!("*");
     for txt in 0..(txt.len() + 5) {
