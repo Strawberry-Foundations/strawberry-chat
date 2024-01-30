c7f3a9ce12a2311de0bbc2a1b5437de1a08f3acc
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-30 20:35:23 +0000

    Prevention of sending empty messages (#137)

--- src/security/automod.rs
+++ src/security/automod.rs
@@ -17,12 +17,18 @@ pub enum MessageAction {
 impl MessageVerification {
     pub fn check(&self, content: &impl ToString) -> MessageAction {
         let content = content.to_string();
+
         if self.blocked_words.blacklisted_words.iter().any(|w| content.contains(w)) {
             return MessageAction::Kick;
         }
         if self.blocked_words.content_block.iter().any(|w| content.contains(w)) {
             return MessageAction::Hide;
         }
+
+        if content.is_empty() {
+            return MessageAction::Hide;
+        }
+
         MessageAction::Allow
     }
 }
