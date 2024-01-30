61044a8c31f3a0d0ee685ca9454b36fac75866a7
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-30 16:15:01 +0000

    Fixed online users bug

--- src/commands/user/online.rs
+++ src/commands/user/online.rs
@@ -1,22 +1,23 @@
 use stblib::colors::{BOLD, C_RESET, CYAN, GREEN, UNDERLINE};
 
 use crate::global::CONFIG;
-use crate::system_core::{commands, CORE};
+use crate::system_core::commands;
 use crate::system_core::commands::CommandCategory;
 use crate::system_core::message::MessageToClient;
 use crate::system_core::server_core::get_online_usernames;
-// use crate::system_core::server_core::{get_online_users};
 
 pub fn online() -> commands::Command {
     async fn logic(ctx: &commands::Context) -> commands::CommandResponse {
+        let online_users_vec = get_online_usernames().await.clone();
+
         let online_users = if CONFIG.config.max_users == -1 {
-            format!("{}", CORE.read().await.online_users)
+            format!("{}", online_users_vec.len())
         } else {
-            format!("{}/{}", CORE.read().await.online_users, CONFIG.config.max_users)
+            format!("{}/{}", online_users_vec.len(), CONFIG.config.max_users)
         };
 
         let message = format!("{GREEN}{BOLD}{UNDERLINE}Users who are currently online ({online_users}){C_RESET}
-        {BOLD}->{C_RESET} {CYAN}{}{C_RESET}", get_online_usernames().await.join(", ")
+        {BOLD}->{C_RESET} {CYAN}{}{C_RESET}", online_users_vec.join(", ")
         );
 
         ctx.tx_channel.send(MessageToClient::SystemMessage {
