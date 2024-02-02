ac1fa13b76a66474aa766fd2c29fe74ec731047f
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-02 21:37:13 +0000

    Fixed online mode

--- src/security/online_mode.rs
+++ src/security/online_mode.rs
@@ -1,5 +1,5 @@
 use reqwest::Client;
-use crate::global::{API, LOGGER};
+use crate::global::{API, LOGGER, RUNTIME_LOGGER};
 
 
 #[derive(Default)]
@@ -10,20 +10,27 @@ pub struct OnlineMode {
 }
 
 impl OnlineMode {
-    pub fn new() -> Self {
-        Self::default()
+    pub fn new(enabled: bool) -> Self {
+        Self {
+            enabled,
+            ..Default::default()
+        }
     }
 
     pub async fn auth(&self) {
         if self.enabled {
             let client = Client::new();
 
-            let Ok(response) = client.get(format!("{API}utils/user/ip")).send().await else { return };
+            let Ok(response) = client.get(format!("{API}utils/user/ip")).send().await else {
+                LOGGER.error("Could not send request to Strawberry API");
+                return
+            };
 
             if response.status().is_success() {
                 let Ok(body) = response.text().await else { return };
+                let json: serde_json::Value = serde_json::from_str(body.as_str()).unwrap();
 
-                LOGGER.info(format!("Connected to Strawberry API with IP {body}"));
+                RUNTIME_LOGGER.info(format!("Connected to Strawberry API with IP {}", json["ip"].as_str().unwrap()));
             }
             else {
                 LOGGER.error("Error while connecting to Strawberry API");
