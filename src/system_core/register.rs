7012b7ff74a8c14f299492f27c34f3dbd4716670
Juliandev02 <juli.mackenstein@gmail.com>
2024-03-02 17:21:56 +0100

    finalized username + password verification

--- src/system_core/register.rs
+++ src/system_core/register.rs
@@ -5,10 +5,13 @@ use stblib::stbm::stbchat::packet::ClientPacket;
 use stblib::utilities::contains_whitespace;
 use tokio::io::{AsyncWriteExt, WriteHalf};
 use tokio::net::TcpStream;
+use crate::constants::chars::USERNAME_ALLOWED_CHARACTERS;
 use crate::constants::log_messages::{DISCONNECTED, S2C_ERROR, WRITE_PACKET_FAIL};
-use crate::global::{LOGGER, MESSAGE_VERIFICATOR};
+use crate::database::db::DATABASE;
+use crate::global::{CONFIG, LOGGER, MESSAGE_VERIFICATOR};
 use crate::security::verification::MessageAction;
 use crate::system_core::log::log_parser;
+use crate::utilities::is_valid_username;
 
 pub async fn client_register(
     username: String,
@@ -17,28 +20,89 @@ pub async fn client_register(
     w_client: &mut OutgoingPacketStream<WriteHalf<TcpStream>>,
     peer_addr: IpAddr
 ) {
+
+    /// Check if the username is in blacklisted words
     let action = MESSAGE_VERIFICATOR.check(&username.to_lowercase());
 
     match action {
         MessageAction::Kick | MessageAction::Hide => {
-            w_client.write(ClientPacket::SystemMessage { 
-                message: format!("{YELLOW}{BOLD}This username is not allowed!{C_RESET}") 
+            w_client.write(ClientPacket::SystemMessage {
+                message: format!("{YELLOW}{BOLD}This username is not allowed!{C_RESET}")
             }).await.unwrap_or_else(|_| LOGGER.warning(WRITE_PACKET_FAIL));
 
             LOGGER.info(log_parser(DISCONNECTED, &[&peer_addr]));
-            w_client.inner_mut().shutdown().await.unwrap_or_else(|_| LOGGER.error(format!("{S2C_ERROR} (core::login::#85)")));
+            w_client.inner_mut().shutdown().await.unwrap_or_else(|_| LOGGER.error(format!("{S2C_ERROR} (core::register::#31)")));
         },
         MessageAction::Allow => { }
     }
 
+    /// If username is in this set of blacklisted words, return an error message
+    if ["exit", "register", "login", "sid"].contains(&username.as_str()) {
+        w_client.write(ClientPacket::SystemMessage {
+            message: format!("{YELLOW}{BOLD}This username is not allowed!{C_RESET}")
+        }).await.unwrap_or_else(|_| LOGGER.warning(WRITE_PACKET_FAIL));
+
+        LOGGER.info(log_parser(DISCONNECTED, &[&peer_addr]));
+        w_client.inner_mut().shutdown().await.unwrap_or_else(|_| LOGGER.error(format!("{S2C_ERROR} (core::register)")));
+    }
+
+    /// If username contains whitespaces, return an error message
     if contains_whitespace(&username) {
         w_client.write(ClientPacket::SystemMessage {
             message: format!("{YELLOW}{BOLD}Your username must not contain spaces{C_RESET}")
         }).await.unwrap_or_else(|_| LOGGER.warning(WRITE_PACKET_FAIL));
 
         LOGGER.info(log_parser(DISCONNECTED, &[&peer_addr]));
-        w_client.inner_mut().shutdown().await.unwrap_or_else(|_| LOGGER.error(format!("{S2C_ERROR} (core::login::#85)")));
+        w_client.inner_mut().shutdown().await.unwrap_or_else(|_| LOGGER.error(format!("{S2C_ERROR} (core::register)")));
     }
-    
-    if !
+
+    /// If username character is not in our charset, return an error message
+    if !is_valid_username(username.as_str(), USERNAME_ALLOWED_CHARACTERS) {
+        w_client.write(ClientPacket::SystemMessage {
+            message: format!("{YELLOW}{BOLD}Please use only letters, numbers, dots or underscores{C_RESET}")
+        }).await.unwrap_or_else(|_| LOGGER.warning(WRITE_PACKET_FAIL));
+
+        LOGGER.info(log_parser(DISCONNECTED, &[&peer_addr]));
+        w_client.inner_mut().shutdown().await.unwrap_or_else(|_| LOGGER.error(format!("{S2C_ERROR} (core::register)")));
+    }
+
+    /// If username is longer than max_username_length (default: 32) characters, return an error message
+    if username.len() > CONFIG.config.max_username_length as usize {
+        w_client.write(ClientPacket::SystemMessage {
+            message: format!("{YELLOW}{BOLD}Your username is too long{C_RESET}")
+        }).await.unwrap_or_else(|_| LOGGER.warning(WRITE_PACKET_FAIL));
+
+        LOGGER.info(log_parser(DISCONNECTED, &[&peer_addr]));
+        w_client.inner_mut().shutdown().await.unwrap_or_else(|_| LOGGER.error(format!("{S2C_ERROR} (core::register)")));
+    }
+
+    /// Check if username is already taken
+    if DATABASE.is_username_taken(username).await {
+        w_client.write(ClientPacket::SystemMessage {
+            message: format!("{YELLOW}{BOLD}This username is already in use!{C_RESET}")
+        }).await.unwrap_or_else(|_| LOGGER.warning(WRITE_PACKET_FAIL));
+
+        LOGGER.info(log_parser(DISCONNECTED, &[&peer_addr]));
+        w_client.inner_mut().shutdown().await.unwrap_or_else(|_| LOGGER.error(format!("{S2C_ERROR} (core::register)")));
+    }
+
+    /// If password contains whitespaces, return an error message
+    if contains_whitespace(&password) {
+        w_client.write(ClientPacket::SystemMessage {
+            message: format!("{YELLOW}{BOLD}Your password must not contain spaces{C_RESET}")
+        }).await.unwrap_or_else(|_| LOGGER.warning(WRITE_PACKET_FAIL));
+
+        LOGGER.info(log_parser(DISCONNECTED, &[&peer_addr]));
+        w_client.inner_mut().shutdown().await.unwrap_or_else(|_| LOGGER.error(format!("{S2C_ERROR} (core::register)")));
+    }
+
+    /// If password is longer than max_password_length (default: 256) characters, return an error message
+    if password.len() > CONFIG.config.max_password_length as usize {
+        w_client.write(ClientPacket::SystemMessage {
+            message: format!("{YELLOW}{BOLD}Your password is too long{C_RESET}")
+        }).await.unwrap_or_else(|_| LOGGER.warning(WRITE_PACKET_FAIL));
+
+        LOGGER.info(log_parser(DISCONNECTED, &[&peer_addr]));
+        w_client.inner_mut().shutdown().await.unwrap_or_else(|_| LOGGER.error(format!("{S2C_ERROR} (core::register)")));
+    }
 }
