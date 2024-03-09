f757d62ad66b564d86fa4f9f7813f4095d4d0517
Juliandev02 <julian@strawberryfoundations.xyz>
2024-03-09 22:24:30 +0100

    Finalized direct messages

--- src/commands/default/dm.rs
+++ src/commands/default/dm.rs
@@ -1,15 +1,78 @@
-use stblib::colors::{BOLD, C_RESET, WHITE};
+use sqlx::Row;
+use stblib::colors::{BOLD, C_RESET, GRAY, RED, YELLOW};
+use stblib::utilities::escape_ansi;
+use crate::database::db::DATABASE;
+use crate::global::LOGGER;
 
-use crate::global::CONFIG;
 use crate::system_core::commands;
 use crate::system_core::commands::CommandCategory;
 use crate::system_core::message::MessageToClient;
+use crate::system_core::server_core::get_senders_by_username_ignore_case;
+use crate::utilities::role_color_parser;
 
 pub fn dm_basic() -> commands::Command {
     async fn logic(ctx: &commands::Context) -> commands::CommandResponse {
+        if ctx.args.len() < 2 {
+            return Ok(Some(
+                format!(
+                    "{BOLD}{RED}Command requires 2 arguments - but only {} were given{C_RESET}",
+                    ctx.args.len()
+                )))
+        }
+
+        let conn = get_senders_by_username_ignore_case(ctx.args[0].as_str()).await;
+
+        if conn.is_empty() {
+            return Ok(Some(format!("{BOLD}{RED}User not found or offline{C_RESET}")))
+        }
+
+        if ctx.executor.username == ctx.args[0].as_str() {
+            return Ok(Some(format!("{BOLD}{YELLOW}You cannot send yourself a message!{C_RESET}")))
+        }
+
+        let data = sqlx::query("SELECT * FROM users WHERE username = ?")
+            .bind(ctx.args[0].as_str())
+            .fetch_all(&DATABASE.connection)
+            .await.expect("err");
+
+        let enable_dms: bool = data.first().unwrap().get("enable_dms");
+
+        if !enable_dms {
+            return Ok(Some(format!("{BOLD}{YELLOW}This user has deactivated his/her DMs{C_RESET}")))
+        }
+
+        let role_color: String = role_color_parser(data.first().unwrap().get("role_color"));
+        let message: Vec<String> = ctx.args[1..].to_vec();
+
         ctx.tx_channel.send(MessageToClient::SystemMessage {
-            content: format!("{WHITE}{BOLD}{}{C_RESET}", CONFIG.server.description)
+            content: format!(
+                "{}You{C_RESET}{GRAY} --> {role_color}{}{C_RESET}: {}",
+                ctx.executor.role_color,
+                ctx.args[0],
+                message.join(" ")
+            )
         }).await.unwrap();
+        
+        for tx in conn {
+            tx.send(MessageToClient::SystemMessage {
+                content: format!(
+                    "{}{}{C_RESET}{GRAY} --> {role_color}You{C_RESET}: {}",
+                    ctx.executor.role_color,
+                    ctx.executor.username,
+                    message.join(" ")
+                )
+            }).await.unwrap();
+
+            tx.send(MessageToClient::Notification {
+                title: String::from("Strawberry Chat (Direct Message)"),
+                username: format!("@{}", ctx.executor.username.clone()),
+                avatar_url: ctx.executor.avatar_url.clone(),
+                content: escape_ansi(&message.join(" ")),
+                bell: false,
+            }).await.unwrap_or_else(|e| {
+                LOGGER.error(format!("Failed to send internal packet: {e}"));
+            });
+        }
 
         Ok(None)
     }
