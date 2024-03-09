f8727c804d8a046cebcbc1aebdd35795f6165359
Juliandev02 <julian@strawberryfoundations.xyz>
2024-03-10 00:06:34 +0100

    better code formatting (Preparing for release)

--- src/system_core/watchdog.rs
+++ src/system_core/watchdog.rs
@@ -1,9 +1,11 @@
 //! Checks if the core thread is running, stops the server if not
 
 use std::time::Duration;
+
 use tokio::spawn;
 use tokio::sync::mpsc::{channel, Receiver};
 use tokio::time::sleep;
+
 use crate::CORE_HANDLE;
 use crate::global::LOGGER;
 use crate::system_core::server_core::core_thread;
@@ -11,9 +13,11 @@ use crate::system_core::server_core::core_thread;
 pub async fn watchdog_thread(mut rx: Receiver<()>) {
     loop {
         sleep(Duration::from_secs(4)).await;
+        
         if rx.try_recv().is_err() {
             LOGGER.warning("Core thread froze/closed, restarting it...");
             CORE_HANDLE.get().unwrap().lock().unwrap().abort();
+            
             let (wd_tx, wd_rx) = channel::<()>(1);
             rx = wd_rx;
             *CORE_HANDLE.get().unwrap().lock().unwrap() = spawn(core_thread(wd_tx));
