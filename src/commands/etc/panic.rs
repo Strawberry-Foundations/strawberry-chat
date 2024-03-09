dff876cf7b90475a1149c04bbe9e07654f7334bf
Juliandev02 <julian@strawberryfoundations.xyz>
2024-03-09 23:19:19 +0100

    added permissions

--- src/commands/etc/panic.rs
+++ src/commands/etc/panic.rs
@@ -1,5 +1,6 @@
 use crate::system_core::commands;
 use crate::system_core::commands::CommandCategory;
+use crate::system_core::permissions::Permissions;
 
 pub fn panic_command() -> commands::Command {
     fn logic(_: &commands::Context) -> commands::CommandResponse {
@@ -11,6 +12,7 @@ pub fn panic_command() -> commands::Command {
         aliases: vec![],
         description: "Panics the core thread - DEBUG ONLY".to_string(),
         category: CommandCategory::Etc,
+        permissions: Permissions::Admin,
         handler: |ctx| Box::pin(async move {
             logic(&ctx)
         }),
