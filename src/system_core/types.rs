826b560a91447280c00721a0227b10e9042fd0cd
Juliandev02 <julian@strawberryfoundations.xyz>
2024-03-10 00:04:07 +0100

    Removed unused code, preparing for release

--- src/system_core/types.rs
+++ src/system_core/types.rs
@@ -1,12 +1,1 @@
-pub const SYSTEM_MESSAGE: &str = "system_message";
-pub const USER_MESSAGE: &str = "user_message";
-
-pub const STBCHAT_NOTIFICATION: &str = "stbchat_notification";
-pub const STBCHAT_BACKEND: &str = "stbchat_backend";
-pub const STBCHAT_LOGIN: &str = "stbchat_login";
-pub const STBCHAT_REGISTER: &str = "stbchat_register";
-pub const STBCHAT_EVENT: &str = "stbchat_event";
-
-pub const LOGIN_EVENT: &str = "event.login";
-
 pub const CRTLCODE_CLIENT_EXIT: &str = "crtl.client.exit";
