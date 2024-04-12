fde23ee2661229eaaa4f24391fcc7c7f07075b62
Juliandev02 <julian@strawberryfoundations.xyz>
2024-04-12 20:50:28 +0200

    update: updated user settings help message

--- src/commands/user/user_settings.rs
+++ src/commands/user/user_settings.rs
@@ -1,27 +1,36 @@
-use stblib::colors::{BOLD, C_RESET, RED};
+use stblib::colors::{BOLD, C_RESET, LIGHT_GREEN, RED};
 use crate::constants::messages::USER_SETTINGS_HELP;
+use crate::database::db::DATABASE;
 
 use crate::system_core::commands;
 use crate::system_core::commands::CommandCategory;
-use crate::system_core::internals::MessageToClient;
 use crate::system_core::permissions::Permissions;
+use crate::utilities::{bool_color_fmt, string_to_bool};
 
 pub fn user_settings() -> commands::Command {
     async fn logic(ctx: &commands::Context) -> commands::CommandResponse {
         if ctx.args.is_empty() {
-            ctx.tx_channel.send(MessageToClient::SystemMessage {
-                content: format!("{RED}{BOLD}Missing arguments - Command requires at least 1 argument - Got 0 arguments{C_RESET}")
-            }).await.unwrap();
-        }
-        
-        match ctx.args[0].as_str() {
-            "help" => ctx.tx_channel.send(
-                    MessageToClient::SystemMessage { content: USER_SETTINGS_HELP.to_string() })
-                    .await.unwrap(),
-            _ => todo!(),
+            return Ok(Some(format!("{RED}{BOLD}Missing arguments - Command requires at least 1 argument - Got 0 arguments{C_RESET}")))
         }
 
-        Ok(None)
+        match ctx.args[0].as_str() {
+            "help" => Ok(Some(USER_SETTINGS_HELP.to_string())),
+            "enable-dms" => {
+                if ctx.args[1].is_empty() {
+                    return Ok(Some(format!("{RED}{BOLD}Missing arguments - Subcommand requires at least 1 argument - Got 0 arguments{C_RESET}")))
+                }
+
+                sqlx::query("UPDATE users SET enable_dms = ? WHERE username = ?")
+                    .bind(string_to_bool(&ctx.args[1]))
+                    .bind(&ctx.executor.username)
+                    .execute(&DATABASE.connection)
+                    .await
+                    .unwrap_or_else(|err| { println!("{err}"); std::process::exit(1) });
+
+                Ok(Some(format!("{LIGHT_GREEN}Updated enable_dms to {}", bool_color_fmt(string_to_bool(&ctx.args[1])))))
+            },
+            _ => Ok(Some(format!("{RED}{BOLD}Invalid subcommand!{C_RESET}"))),
+        }
     }
 
     commands::Command {
