d50b5f4465ce8ef684c228b2f50f9b652803ed52
Juliandev02 <julian@strawberryfoundations.xyz>
2024-04-12 19:27:26 +0200

    fix: Fixed breaking changes from stblib update

--- src/system_core/config.rs
+++ src/system_core/config.rs
@@ -79,7 +79,7 @@ impl GlobalConfig {
         let cfg_content = utilities::open_config(&config_path);
 
         let mut config: Self = from_str(&cfg_content).unwrap_or_else(|err| {
-            LOGGER.panic_critical(format!("Could not read configuration: {err}"));
+            LOGGER.critical_panic(format!("Could not read configuration: {err}"));
         });
 
         config.path = config_path;
