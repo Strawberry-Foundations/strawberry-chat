09a3b4b3e1078fb0e7bacbf44aab451e4e40eb2e
Juliandev02 <julian@strawberryfoundations.xyz>
2024-04-12 20:42:32 +0200

    update: updated user settings help message

--- src/constants/messages.rs
+++ src/constants/messages.rs
@@ -1,7 +1,6 @@
-pub const USER_SETTINGS_HELP: &str = "
-        \x1B[36m\x1B[4m\x1B[1mUser Settings\x1B[0m\x1B[0m
+pub const USER_SETTINGS_HELP: &str = "\x1B[36m\x1B[4m\x1B[1mUser Settings\x1B[0m\x1B[0m
         \x1B[34m\x1B[1m/settings help: \x1B[0mHelp Command
-        \x1B[34m\x1B[1m/settings enable_dms <true/false>: \x1B[0mEnable or disable your dms
+        \x1B[34m\x1B[1m/settings enable-dms <true/false>: \x1B[0mEnable or disable your dms
         \x1B[34m\x1B[1m/settings discord_name <discord_name>: \x1B[0mChange your discord name
         \x1B[34m\x1B[1m/settings role_color <color>: \x1B[0mChange your role color
             -> Available Colors: <(light/bold)><red/green/cyan/blue/yellow/magenta>
