3f66bf9b61b93a3132a0400af012d4e5a4e24f2a
Juliandev02 <julian@strawberryfoundations.xyz>
2024-04-12 22:08:10 +0200

    impl: Made send_to_all public

--- src/system_core/server_core.rs
+++ src/system_core/server_core.rs
@@ -148,7 +148,7 @@ async fn get_events() -> Vec<(Event, usize)> {
 }
 
 #[allow(clippy::significant_drop_in_scrutinee)]
-async fn send_to_all(what: MessageToClient, authed_only: bool) {
+pub async fn send_to_all(what: MessageToClient, authed_only: bool) {
     for conn in CLIENTS.write().await.iter_mut().filter(|conn| !authed_only || conn.is_auth()) {
         let _ = conn.tx.send(what.clone()).await;
     }
