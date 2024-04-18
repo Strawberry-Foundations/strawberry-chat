ea478b07c7a54ff37829fc0c714688dc74d0d298
Juliandev02 <julian@strawberryfoundations.xyz>
2024-04-18 19:38:38 +0200

    cmd: Added delete account

--- src/commands/etc/delete_account.rs
+++ src/commands/etc/delete_account.rs
@@ -1,17 +1,85 @@
-use stblib::colors::{BOLD, C_RESET, YELLOW};
+use stblib::colors::{BOLD, C_RESET, RED, YELLOW};
+use stblib::utilities::escape_ansi;
+use tokio::spawn;
+use crate::database::db::DATABASE;
 
 use crate::system_core::commands;
 use crate::system_core::commands::CommandCategory;
+use crate::system_core::hooks::Hook;
 use crate::system_core::internals::MessageToClient;
 use crate::system_core::permissions::Permissions;
+use crate::system_core::server_core::Event;
 
 pub fn delete_account() -> commands::Command {
     async fn logic(ctx: &commands::Context) -> commands::CommandResponse {
         ctx.tx_channel.send(MessageToClient::SystemMessage {
             content: format!("{YELLOW}{BOLD}Are you sure you want to delete your user account? This action is irreversible!!{C_RESET}")
         }).await.unwrap();
-        
-        
+
+        let mut hook = Hook::new(ctx.executor.clone(), ctx.tx_channel.clone(), 1).await;
+        let mut hook2 = Hook::new(ctx.executor.clone(), ctx.tx_channel.clone(), 1).await;
+        let mut hook3 = Hook::new(ctx.executor.clone(), ctx.tx_channel.clone(), 1).await;
+        let mut hook4 = Hook::new(ctx.executor.clone(), ctx.tx_channel.clone(), 1).await;
+
+        spawn(async move {
+            if let Some(Event::UserMessage { content, .. }) = hook.rx.recv().await {
+                // First time for confirming account deletion
+                if escape_ansi(&content).eq_ignore_ascii_case("yes") {
+                    hook.tx_ctx.send(MessageToClient::SystemMessage {
+                        content: format!("{RED}{BOLD}THIS IS YOUR VERY LAST WARNING! This action is irreversible!! ARE YOU SURE?{C_RESET}")
+                    }).await.unwrap();
+
+
+                    if let Some(Event::UserMessage { content, ..}) = hook2.rx.recv().await {
+                        // Second time for confirming account deletion
+                        if escape_ansi(&content).eq_ignore_ascii_case("yes") {
+                            hook.tx_ctx.send(MessageToClient::SystemMessage {
+                                content: format!("{YELLOW}{BOLD}Enter your username to confirm the deletion of your account:{C_RESET}")
+                            }).await.unwrap();
+
+                            if let Some(Event::UserMessage { author, content}) = hook3.rx.recv().await {
+                                // Third time for confirming account deletion
+                                if escape_ansi(&content) == author.username {
+                                    hook.tx_ctx.send(MessageToClient::SystemMessage {
+                                        content: format!("{YELLOW}{BOLD}Now, enter your password to finally confirm your account deletion:{C_RESET}")
+                                    }).await.unwrap();
+
+                                    if let Some(Event::UserMessage { author, content}) = hook4.rx.recv().await {
+                                        // Last time for confirming account deletion
+                                        let (_, result) = DATABASE.check_credentials(&author.username, &content).await;
+
+                                        if result {
+                                            hook.tx_ctx.send(MessageToClient::SystemMessage {
+                                                content: format!("{YELLOW}{BOLD}Deleting your user account...{C_RESET}")
+                                            }).await.unwrap();
+
+                                        } else {
+                                            hook.tx_ctx.send(MessageToClient::SystemMessage {
+                                                content: format!("{YELLOW}{BOLD}Deletion of your account has been canceled...{C_RESET}")
+                                            }).await.unwrap();
+                                        }
+                                    }
+                                } else {
+                                    hook.tx_ctx.send(MessageToClient::SystemMessage {
+                                        content: format!("{YELLOW}{BOLD}Deletion of your account has been canceled...{C_RESET}")
+                                    }).await.unwrap();
+                                }
+                            }
+                        } else {
+                            hook.tx_ctx.send(MessageToClient::SystemMessage {
+                                content: format!("{YELLOW}{BOLD}Deletion of your account has been canceled...{C_RESET}")
+                            }).await.unwrap();
+                        }
+                    }
+                } else {
+                    hook.tx_ctx.send(MessageToClient::SystemMessage {
+                        content: format!("{YELLOW}{BOLD}Deletion of your account has been canceled...{C_RESET}")
+                    }).await.unwrap();
+                }
+            }
+        });
+
+
 
         Ok(None)
     }
@@ -20,7 +88,7 @@ pub fn delete_account() -> commands::Command {
         name: "deleteaccount".to_string(),
         aliases: vec!["delaccount"],
         description: "Delete your account on this server".to_string(),
-        category: CommandCategory::User,
+        category: CommandCategory::Etc,
         permissions: Permissions::Member,
         handler: |ctx| Box::pin(async move {
             logic(&ctx).await
