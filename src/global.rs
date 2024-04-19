00aacbd2fdcf9d83c376dbb774e282a4d30bab36
Juliandev02 <julian@strawberryfoundations.xyz>
2024-04-19 17:13:17 +0200

    proj: Updated versions, preparing for v1.11.0 stable

--- src/global.rs
+++ src/global.rs
@@ -13,7 +13,7 @@ use crate::security::online_mode::OnlineMode;
 pub const API: &str = "https://api.strawberryfoundations.xyz/v1/";
 
 pub const BASE_VERSION: &str = "1.11.0";
-pub const ADDITION_VER: &str = "final1";
+pub const ADDITION_VER: &str = "";
 pub const STBM_VER: &str = "3";
 pub const CONFIG_VER: &str  = "9";
 
