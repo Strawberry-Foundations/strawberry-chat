af976c02efa69481988260733927f6bce0a3d70d
Juliandev02 <julian@strawberryfoundations.xyz>
2024-04-25 19:48:50 +0200

    impl: Added user status parser

--- src/commands/user/status.rs
+++ src/commands/user/status.rs
@@ -1,15 +1,20 @@
+use stblib::colors::{BOLD, C_RESET, GREEN};
+
 use crate::system_core::commands;
 use crate::system_core::commands::CommandCategory;
 use crate::system_core::permissions::Permissions;
 use crate::system_core::server_core::STATUS;
 use crate::system_core::status::Status;
+use crate::utilities::parse_user_status;
 
 pub fn status() -> commands::Command {
     async fn logic(ctx: &commands::Context) -> commands::CommandResponse {
-        // STATUS.write().await.append(ctx.executor.username.as_str(), Status::Afk);
+        if ctx.args.is_empty() {
+            let status = *STATUS.read().await.get_by_name(ctx.executor.username.as_str());
+            return Ok(Some(format!("{BOLD}{GREEN}Your current status: {}{C_RESET}", parse_user_status(status, true))))
+        }
 
-        let status = *STATUS.read().await.get_by_name(ctx.executor.username.as_str());
-        Ok(Some(format!("{status:?}")))
+        Ok(None)
     }
 
     commands::Command {
