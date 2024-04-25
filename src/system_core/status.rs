3e0039e2a3d2378a247a5516b1d488345a22b3a6
Juliandev02 <julian@strawberryfoundations.xyz>
2024-04-25 19:25:18 +0200

    impl: added user status system

---
+++ src/system_core/status.rs
@@ -0,0 +1,29 @@
+use std::collections::HashMap;
+
+pub struct UserStatus {
+    pub users: HashMap<String, Status>
+}
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub enum Status {
+    Online,
+    DoNotDisturb,
+    Afk,
+    Offline
+}
+
+impl UserStatus {
+    pub fn new() -> Self {
+        Self {
+            users: HashMap::new()
+        }
+    }
+
+    pub fn append(&mut self, username: &str, status: Status) {
+        self.users.insert(username.to_string(), status).unwrap();
+    }
+
+    pub fn get_by_name(&self, username: &str) -> &Status {
+        self.users.get(username).unwrap()
+    }
+}
