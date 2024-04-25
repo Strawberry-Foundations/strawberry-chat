8fdb74efaeadee62d286e4b5c4ca4acda2a83e8d
Juliandev02 <julian@strawberryfoundations.xyz>
2024-04-25 20:27:39 +0200

    impl: Added ignore case for status system

--- src/system_core/status.rs
+++ src/system_core/status.rs
@@ -21,14 +21,14 @@ impl UserStatus {
     }
 
     pub fn append(&mut self, username: &str, status: Status) {
-        self.users.insert(username.to_string(), status);
+        self.users.insert(username.to_lowercase(), status);
     }
 
     pub fn remove(&mut self, username: &str) {
-        self.users.remove(&username.to_string());
+        self.users.remove(&username.to_lowercase());
     }
 
     pub fn get_by_name(&self, username: &str) -> &Status {
-        self.users.get(username).unwrap_or(&Status::Offline)
+        self.users.get(username.to_lowercase().as_str()).unwrap_or(&Status::Offline)
     }
 }
