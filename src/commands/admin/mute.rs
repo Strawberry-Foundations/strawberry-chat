1c469e13e2c744940bafa7dfaabc55e409cce25c
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-14 19:47:41 +0200

    fix: Fixed command due to breaking changes

--- src/commands/admin/mute.rs
+++ src/commands/admin/mute.rs
@@ -60,6 +60,7 @@ pub fn mute() -> commands::Command {
         description: "Mutes a user".to_string(),
         category: CommandCategory::Admin,
         permissions: Permissions::Admin,
+        required_args: 0,
         handler: |ctx| Box::pin(async move {
             logic(&ctx).await
         }),
