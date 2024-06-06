65b95b127924b5fcffec48efdb0b452bb77da0e1
Juliandev02 <julian@strawberryfoundations.xyz>
2024-06-06 21:18:27 +0200

    impl: Added ADMIN_SETTINGS_HELP

--- src/constants/messages.rs
+++ src/constants/messages.rs
@@ -9,3 +9,9 @@ pub const USER_SETTINGS_HELP: &str = "\x1B[36m\x1B[4m\x1B[1mUser Settings\x1B[0m
         \x1B[34m\x1B[1m/settings account username <new username>: \x1B[0mChange your current username
         \x1B[34m\x1B[1m/settings account password <new password>: \x1B[0mChange your current password
     ";
+
+pub const ADMIN_SETTINGS_HELP: &str = "\x1B[36m\x1B[4m\x1B[1mAdmin Settings\x1B[0m\x1B[0m
+        \x1B[34m\x1B[1m/admin-settings help: \x1B[0mHelp Command
+        \x1B[34m\x1B[1m/admin-settings role <user> <role>: \x1B[0mSet the role for a user
+        \x1B[34m\x1B[1m/admin-settings badge add/remove <user> <badge>: \x1B[0mAdd a badge to a user
+    ";
