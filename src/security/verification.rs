09328e07579e498caa46380ceb6a13022100fc21
Juliandev02 <julian@strawberryfoundations.xyz>
2024-06-16 15:04:52 +0200

    impl: Added blacklisted words

--- src/security/verification.rs
+++ src/security/verification.rs
@@ -1,14 +1,21 @@
+use std::fs::File;
+use std::io::Read;
+use std::path::{Path, PathBuf};
+use serde::Deserialize;
 use sqlx::Row;
 use stblib::stbchat::object::User;
-use crate::database::db::DATABASE;
 
-pub struct BlockedWords<'a> {
-    pub blacklisted_words: Vec<&'a str>,
-    pub content_block: Vec<&'a str>,
+use crate::database::db::DATABASE;
+use crate::global::LOGGER;
+
+#[derive(Deserialize)]
+pub struct BlockedWords {
+    pub blacklist: Vec<String>,
+    pub content_block: Vec<String>,
 }
 
 pub struct MessageVerification {
-    pub blocked_words: BlockedWords<'static>,
+    pub bad_words: BlockedWords,
 }
 
 #[derive(PartialEq, Eq)]
@@ -23,10 +30,10 @@ impl MessageVerification {
     pub fn check(&self, content: &impl ToString) -> MessageAction {
         let content = content.to_string();
 
-        if self.blocked_words.blacklisted_words.iter().any(|w| content.contains(w)) {
+        if self.bad_words.blacklist.iter().any(|w| content.contains(w)) {
             return MessageAction::Kick;
         }
-        if self.blocked_words.content_block.iter().any(|w| content.contains(w)) {
+        if self.bad_words.content_block.iter().any(|w| content.contains(w)) {
             return MessageAction::Hide;
         }
         if content.trim().is_empty() {
@@ -45,10 +52,10 @@ impl MessageVerification {
 
         let muted: bool = user_data.first().unwrap().get("muted");
 
-        if self.blocked_words.blacklisted_words.iter().any(|w| content.contains(w)) {
+        if self.bad_words.blacklist.iter().any(|w| content.contains(w)) {
             return MessageAction::Kick;
         }
-        if self.blocked_words.content_block.iter().any(|w| content.contains(w)) {
+        if self.bad_words.content_block.iter().any(|w| content.contains(w)) {
             return MessageAction::Hide;
         }
         if content.trim().is_empty() {
@@ -64,11 +71,31 @@ impl MessageVerification {
 
 impl MessageVerification {
     pub fn new() -> Self {
+        let exe_path = std::env::current_exe().unwrap();
+        let exe_dir = exe_path.parent().unwrap();
+        let exe_dir_str = PathBuf::from(exe_dir).display().to_string();
+
+        let mut bad_words_path = format!("{exe_dir_str}/config.yml");
+
+        if !Path::new(&bad_words_path).exists() {
+            bad_words_path = String::from("./bad_words.yml");
+        }
+
+        let mut file = File::open(bad_words_path).unwrap_or_else(|err| {
+           LOGGER.critical_panic(format!("Could not read bad words: {err}"))
+        });
+
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap_or_else(|err| {
+            LOGGER.critical_panic(format!("Could not read bad words: {err}"))
+        });
+        
+        let bad_words: BlockedWords = serde_yaml::from_str(&contents).unwrap_or_else(|err| {
+            LOGGER.critical_panic(format!("Could not read bad words: {err}"))
+        });
+
         Self {
-            blocked_words: BlockedWords {
-                blacklisted_words: vec!["gullideckel"],
-                content_block: vec!["[#<keepalive.event.sent>]"],
-            }
+            bad_words
         }
     }
 }
