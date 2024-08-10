e74cce470def5a222ec7757a6fccc9458495fb9f
Juliandev02 <julian@strawberryfoundations.xyz>
2024-08-10 14:07:44 +0200

    feat: add unblock command

--- src/commands/user/block.rs
+++ src/commands/user/block.rs
@@ -1,10 +1,11 @@
 use sqlx::Row;
 use stblib::colors::{BOLD, C_RESET, RED, LIGHT_GREEN, GRAY};
-use crate::database::db::DATABASE;
+
 use crate::system_core::commands;
 use crate::system_core::commands::CommandCategory;
 use crate::system_core::internals::MessageToClient;
 use crate::system_core::permissions::Permissions;
+use crate::database::db::DATABASE;
 
 pub fn block() -> commands::Command {
     async fn logic(ctx: &commands::Context) -> commands::CommandResponse {
