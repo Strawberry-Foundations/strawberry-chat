1efd0914f762e9e9e9de0f048a3da6a5371d2032
Juliandev02 <julian@strawberryfoundations.xyz>
2024-09-06 22:44:57 +0200

    feat: update about command

--- src/commands/default/about.rs
+++ src/commands/default/about.rs
@@ -1,18 +1,19 @@
-use stblib::colors::{BLUE, BOLD, C_RESET, GREEN, RESET, UNDERLINE};
+use stblib::colors::{BLUE, BOLD, C_RESET, CYAN, GREEN, RESET, UNDERLINE};
 
 use crate::system_core::commands;
 use crate::system_core::commands::CommandCategory;
 use crate::system_core::internals::MessageToClient;
 use crate::system_core::permissions::Permissions;
-use crate::global::{AUTHORS, CHAT_NAME, CODENAME, DEFAULT_VERSION, EXT_VERSION, SERVER_EDITION};
+use crate::global::{AUTHORS, CHAT_NAME, CODENAME, CORE_VERSION, DEFAULT_VERSION, EXT_VERSION, SERVER_EDITION};
 
 pub fn about() -> commands::Command {
     async fn logic(ctx: &commands::Context) -> commands::CommandResponse {
         let message = format!("{GREEN}{BOLD}{UNDERLINE}About {CHAT_NAME}{C_RESET}
-        {BLUE}{BOLD}Thank you for using {CHAT_NAME}!{C_RESET}
+        {CYAN}{BOLD}Thank you for using {CHAT_NAME}!{C_RESET}
         {BLUE}{BOLD}Version:{RESET} {} {CODENAME} ({SERVER_EDITION}) ({}){RESET}{C_RESET}
+        {BLUE}{BOLD}Core Version:{RESET} {}{RESET}{C_RESET}
         {BLUE}{BOLD}Author:{RESET} {} {RESET}{C_RESET}",
-        DEFAULT_VERSION.clone(), EXT_VERSION.clone(), AUTHORS.join(", "));
+        *DEFAULT_VERSION, *EXT_VERSION, *CORE_VERSION, AUTHORS.join(", "));
 
 
         ctx.tx_channel.send(MessageToClient::SystemMessage {
