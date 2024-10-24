33ccfe2425013d8548e3d4dc897d4a92ca83c38b
Juliandev02 <julian@strawberryfoundations.org>
2024-10-24 17:44:59 +0200

    feat: update rt_logger

--- src/global.rs
+++ src/global.rs
@@ -39,10 +39,10 @@ lazy_static! {
         stblib::logging::featureset::FeatureSet::new(),
         LogFormat {
             info: format!("{C_RESET}{BOLD}{GREEN}STARTUP{C_RESET}  [%<message>%]"),
-            error: format!("{C_RESET}{BOLD}{RED}RUNTIME_ERROR{C_RESET} [%<message>%]"),
+            error: format!("{C_RESET}{BOLD}{RED}ERR{C_RESET}      [%<message>%]"),
             default: format!("{C_RESET}{BOLD}{BLUE}DATABASE{C_RESET} [%<message>%]"),
             warning: format!("{C_RESET}{BOLD}{YELLOW}WARNING{C_RESET} [%<message>%]"),
-            panic: format!("{C_RESET}{BOLD}{YELLOW}PANIC{C_RESET} [%<message>%]"),
+            panic: format!("{C_RESET}{BOLD}{RED}PANIC{C_RESET}    [%<message>%]"),
             critical: String::new(),
             extensions: LogFormatExt {
                 time_fmt: "%Y-%m-%d %H:%M".to_string(),
