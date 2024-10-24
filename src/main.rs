6998dee7a4d4c05270a5e673b473b6de47bb8059
Juliandev02 <julian@strawberryfoundations.org>
2024-10-24 17:25:35 +0200

    feat: remove unused config flags

--- src/main.rs
+++ src/main.rs
@@ -86,7 +86,7 @@ async fn main(){
     let _ = DATABASE.connection.clone();
 
     delete_last_line();
-    RUNTIME_LOGGER.default(format!("Connected to {ITALIC}{CYAN}{}:{}{RESET} (MySQL->{}->{})", CONFIG.database.host, CONFIG.database.port, CONFIG.database.database_name, CONFIG.database.database_table));
+    RUNTIME_LOGGER.default(format!("Connected to {ITALIC}{CYAN}{}:{}{RESET} (MySQL->{}->{})", CONFIG.database.host, CONFIG.database.port, CONFIG.database.database, CONFIG.database.table));
 
 
     RUNTIME_LOGGER.info(
