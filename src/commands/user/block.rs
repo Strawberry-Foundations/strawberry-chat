61eece58ef696267bf84c23af7dcf9b5f674e5b5
Juliandev02 <julian@strawberryfoundations.org>
2024-10-27 21:33:25 +0100

    fix: fix imports

--- src/commands/user/block.rs
+++ src/commands/user/block.rs
@@ -1,4 +1,3 @@
-use sqlx::Row;
 use stblib::colors::{BOLD, C_RESET, RED, LIGHT_GREEN, GRAY, CYAN, GREEN, YELLOW};
 
 use crate::system_core::commands;
@@ -12,10 +11,7 @@ pub fn block() -> commands::Command {
         let user = ctx.args[0].as_str();
 
         if user == "list" {
-            let blocked_users: String = sqlx::query("SELECT blocked FROM users WHERE username = ?")
-                .bind(&ctx.executor.username)
-                .fetch_one(&DATABASE.connection)
-                .await.unwrap().get("blocked");
+            let blocked_users = DATABASE.get_val_from_user(&ctx.executor.username, "blocked").await.unwrap();
 
             if blocked_users.is_empty() {
                 return Err(format!("{BOLD}{YELLOW}You have not yet blocked a user{C_RESET}"))
@@ -40,10 +36,7 @@ pub fn block() -> commands::Command {
             return Err(format!("{BOLD}{RED}Sorry, this user does not exist!{C_RESET}"))
         }
 
-        let blocked_users: String = sqlx::query("SELECT blocked FROM users WHERE username = ?")
-            .bind(&ctx.executor.username)
-            .fetch_one(&DATABASE.connection)
-            .await.unwrap().get("blocked");
+        let blocked_users = DATABASE.get_val_from_user(&ctx.executor.username, "blocked").await.unwrap();
         
         if blocked_users.split(',').any(|x| x == user) {
             return Err(format!("{BOLD}{YELLOW}This user is already blocked{C_RESET}"))
@@ -56,14 +49,7 @@ pub fn block() -> commands::Command {
             format!("{blocked_users}{user},")
         };
 
-        match sqlx::query("UPDATE users SET blocked = ? WHERE username = ?")
-            .bind(new_blocked_users)
-            .bind(&ctx.executor.username)
-            .execute(&DATABASE.connection)
-            .await {
-            Ok(..) => ..,
-            Err(_) => return Err(format!("{BOLD}{RED}Sorry, this user does not exist!{C_RESET}"))
-        };
+        DATABASE.update_val(&ctx.executor.username, "blocked", &new_blocked_users).await;
 
         ctx.tx_channel.send(MessageToClient::SystemMessage {
             content: format!("{BOLD}{LIGHT_GREEN}Blocked {user}. Type {GRAY}/unblock {user}{LIGHT_GREEN} to unblock{C_RESET}")
