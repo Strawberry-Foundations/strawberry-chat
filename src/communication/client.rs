491b56ebf46f0846423ef0408a0bf351e5047922
Juliandev02 <julian@strawberryfoundations.org>
2024-10-29 21:33:01 +0100

    fix: only return user struct

--- src/communication/client.rs
+++ src/communication/client.rs
@@ -52,7 +52,7 @@ pub async fn client_handler(client: TcpStream, rx: Receiver<MessageToClient>, tx
 
     /// # Core (feat): Client Login
     /// Basic feature to verify that you are you (yes)
-    let Some((account, user)) = login::client_login(&mut w_client, &mut r_client, peer_addr).await else {
+    let Some(user) = login::client_login(&mut w_client, &mut r_client, peer_addr).await else {
         tx.send(MessageToServer::RemoveMe {
             username: None
         }).await.unwrap(); // <--- stbbugs::20240202--1: Calls Exception when leaving stbchat while login screen
